eafb4b6963f8aa35f98e6b01912299448b56195e
Misty De Meo <mistydemeo@gmail.com>
2012-12-12 22:23:38 -0800

    doctor: don't use Pathname() on Tiger
    The Pathname() convenience constructor method was added sometime
    after ruby 1.8.2.

--- Library/Homebrew/cmd/doctor.rb
+++ Library/Homebrew/cmd/doctor.rb
@@ -307,7 +307,7 @@ end
 def check_access_usr_local
   return unless HOMEBREW_PREFIX.to_s == '/usr/local'
 
-  unless Pathname('/usr/local').writable_real? then <<-EOS.undent
+  unless Pathname.new('/usr/local').writable_real? then <<-EOS.undent
     The /usr/local directory is not writable.
     Even if this directory was writable when you installed Homebrew, other
     software may change permissions on this directory. Some versions of the
