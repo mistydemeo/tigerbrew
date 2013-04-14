77c102b114234df438a6e70d1567eee7bf52a103
Misty De Meo <mistydemeo@gmail.com>
2013-04-13 22:03:49 -0500

    faac: depends_on :ld64
    Also hacks up libtool to ensure it links correctly.
    
    The right thing to do would probably be to regenerate the autotools, but
    I'd rather skip the extra deps.

--- Library/Formula/faac.rb
+++ Library/Formula/faac.rb
@@ -5,12 +5,21 @@ class Faac < Formula
   url 'http://downloads.sourceforge.net/project/faac/faac-src/faac-1.28/faac-1.28.tar.gz'
   sha1 'd00b023a3642f81bb1fb13d962a65079121396ee'
 
+  # Tiger's ld gives "ld: unknown flag: -compatibility_version"
+  depends_on :ld64
+
   def install
     # This actually breaks faac!
     ['CFLAGS','CPPFLAGS','CXXFLAGS'].each {|f| ENV.remove f, '-faltivec'}
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+
+    # libtool ignores our LDFLAGS :(
+    inreplace "libtool" do |s|
+      s.change_make_var! 'wl', "-B#{Formula.factory('ld64').bin}/ -Wl,"
+    end if MacOS.version < :leopard
+
     system "make install"
   end
 end
