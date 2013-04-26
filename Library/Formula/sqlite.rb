b2650964fbc6029dc9efe64c8ae8c62716ab56b9
Misty De Meo <mistydemeo@gmail.com>
2013-04-25 23:19:35 -0500

    sqlite: disable optimization on Tiger/PPC
    sqlite segfaults when compiled with any optimization on Tiger/PPC.
    Potentially reenable when we upgrade compilers.
    
    Fixes #5.

--- Library/Formula/sqlite.rb
+++ Library/Formula/sqlite.rb
@@ -29,6 +29,9 @@ class Sqlite < Formula
   keg_only :provided_by_osx, "OS X already provides (an older) sqlite3."
 
   def install
+    # sqlite segfaults on PPC Tiger
+    ENV.no_optimization if MacOS.version < :leopard && Hardware::CPU.type == :ppc
+
     ENV.append 'CPPFLAGS', "-DSQLITE_ENABLE_RTREE" unless build.include? "without-rtree"
     ENV.append 'CPPFLAGS', "-DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS" if build.include? "with-fts"
 
