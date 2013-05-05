7242b3f367c8ef6e1dc1073f05f8541683f18fbf
Misty De Meo <mistydemeo@gmail.com>
2013-05-04 20:08:22 -0500

    sdl_image: use common backend on Tiger
    This allows modern versions of sdl_image to compile fine on Tiger,
    since the Mac-specific backend has stuff that was introduced on
    Leopard.

--- Library/Formula/sdl_image.rb
+++ Library/Formula/sdl_image.rb
@@ -2,24 +2,23 @@ require 'formula'
 
 class SdlImage < Formula
   homepage 'http://www.libsdl.org/projects/SDL_image'
-  # SDL_image 1.2.10 is end of the road for Tiger, unless someone wants to
-  # contribute Tiger-specific fixes
-  # Specifically, it uses CGFloat (typedef from 10.5), the
-  # CGColorSpaceModel enum (defined in 10.5), and some CGColorSpace functions (also 10.5)
-  if MacOS.version < :leopard
-    url 'http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.10.tar.gz'
-    sha1 '6bae71fdfd795c3dbf39f6c7c0cf8b212914ef97'
-  else
-    url 'http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz'
-    sha1 '5e3e393d4e366638048bbb10d6a269ea3f4e4cf2'
-  end
+  url 'http://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz'
+  sha1 '5e3e393d4e366638048bbb10d6a269ea3f4e4cf2'
 
   depends_on 'sdl'
+  if MacOS.version < :leopard
+    depends_on 'libpng'
+    depends_on 'jpeg'
+  end
 
   option :universal
 
   def install
     ENV.universal_binary if build.universal?
+
+    # current Mac backend only works on Leopard or newer
+    ENV.append_to_cflags '-DSDL_IMAGE_USE_COMMON_BACKEND' if MacOS.version < :leopard
+
     inreplace 'SDL_image.pc.in', '@prefix@', HOMEBREW_PREFIX
 
     system "./configure", "--prefix=#{prefix}",
