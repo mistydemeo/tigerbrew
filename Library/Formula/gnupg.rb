fd5b80413b8a5e4e89a63816d97eaa27fd15129c
Misty De Meo <mistydemeo@gmail.com>
2013-05-19 17:42:52 -0500

    gnupg: fix IDEA cipher tests on PPC
    Fixes #36.

--- Library/Formula/gnupg.rb
+++ Library/Formula/gnupg.rb
@@ -7,6 +7,9 @@ class Gnupg < Formula
 
   option '8192', 'Build with support for private keys of up to 8192 bits'
 
+  # fixes IDEA tests on PPC; should be fixed in next release
+  def patches; DATA; end
+
   def cflags
     cflags = ENV.cflags.to_s
     cflags += ' -std=gnu89 -fheinous-gnu-extensions' if ENV.compiler == :clang
@@ -16,11 +19,9 @@ class Gnupg < Formula
   def install
     inreplace 'g10/keygen.c', 'max=4096', 'max=8192' if build.include? '8192'
 
-    args = %W[--disable-dependency-tracking --prefix=#{prefix} --disable-asm]
-    # IDEA tests fail on PowerPC
-    args << "--disable-idea" if Hardware::CPU.type == :ppc
-
-    system "./configure", *args
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--disable-asm"
     system "make", "CFLAGS=#{cflags}"
     system "make check"
 
@@ -30,3 +31,27 @@ class Gnupg < Formula
     system "make install"
   end
 end
+
+__END__
+diff -up gnupg-1.4.13/cipher/idea.c.endian gnupg-1.4.13/cipher/idea.c
+--- gnupg-1.4.13/cipher/idea.c.endian	2013-01-07 08:37:48.899033247 -0500
++++ gnupg-1.4.13/cipher/idea.c	2013-01-07 08:39:03.239030102 -0500
+@@ -201,7 +201,7 @@ cipher( byte *outbuf, const byte *inbuf,
+     x2 = *in++;
+     x3 = *in++;
+     x4 = *in;
+-  #ifndef WORDS_BIGENDIAN
++  #ifndef BIG_ENDIAN_HOST
+     x1 = (x1>>8) | (x1<<8);
+     x2 = (x2>>8) | (x2<<8);
+     x3 = (x3>>8) | (x3<<8);
+@@ -234,7 +234,7 @@ cipher( byte *outbuf, const byte *inbuf,
+     MUL(x4, *key);
+ 
+     out = (u16*)outbuf;
+-  #ifndef WORDS_BIGENDIAN
++  #ifndef BIG_ENDIAN_HOST
+     *out++ = (x1>>8) | (x1<<8);
+     *out++ = (x3>>8) | (x3<<8);
+     *out++ = (x2>>8) | (x2<<8);
+
