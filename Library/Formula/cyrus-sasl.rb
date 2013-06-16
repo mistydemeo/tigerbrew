b9d63ab8340b1ad64283edcd51c59595e1ff242e
Misty De Meo <mistydemeo@gmail.com>
2013-06-15 21:10:35 -0500

    cyrus-sasl 2.1.22
    Thanks to @chdiza in #88 for the formula!

---
+++ Library/Formula/cyrus-sasl.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class CyrusSasl < Formula
+  # This formula uses 2.1.22 because, while later versions build on Tiger,
+  # they don't seem to work properly when sending mail
+  homepage 'http://cyrusimap.org'
+  url 'ftp://ftp.cyrusimap.org/cyrus-sasl/OLD-VERSIONS/cyrus-sasl-2.1.22.tar.gz'
+  sha1 'd23454ab12054714ab97d229c86cb934ce63fbb1'
+
+  def install
+
+    system "./configure",
+           "--disable-macos-framework",
+           "--prefix=#{prefix}"
+           # The below are for 2.1.26, which built but wouldn't work
+           # with sending and gmail.  The flags were necessary to get
+           # a succesful build, IIRC.
+           #"--disable-scram",
+           #"--disable-gssapi",
+    system "make"
+    system "make check"
+    system "make install"
+  end
+
+end
+
