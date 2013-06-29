ded1e9178cb8f23095f0f0ac09aaff30bd59a448
Misty De Meo <mistydemeo@gmail.com>
2013-06-29 15:47:11 -0500

    Backport 1.8.7's OpenStruct#new_ostruct_member
    Ruby 1.8.2's OpenStruct has a notable bug that prevents defining
    members that contain exclamation and question marks. The definition
    of the offending method in 1.8.7 has no such bug.

--- Library/Homebrew/extend/tiger.rb
+++ Library/Homebrew/extend/tiger.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 # This file contains a set of monkeypatches to backport modern Ruby
 # features into Tiger's ancient 1.8.2
 
@@ -21,5 +23,26 @@ end
 module Kernel
   def Pathname(target)
     Pathname.new(target)
+  end
+end
+
+# OpenStruct in 1.8.2 uses eval to generate member getters/setters, which
+# results in hilarious syntax errors if a member ends with an exclamation
+# mark or a question mark. e.g.:
+# def #{name}=(x); @table[:#{name}] = x; end
+# Becomes:
+# def all?=(x); @table[:all?] = x; end
+# This notably breaks brew-deps. 1.8.7 more sensibly uses define_method.
+# This method is backported exactly from 1.8.7's definition.
+class OpenStruct
+  def new_ostruct_member(name)
+    name = name.to_sym
+    unless self.respond_to?(name)
+      class << self; self; end.class_eval do
+        define_method(name) { @table[name] }
+        define_method("#{name}=") { |x| modifiable[name] = x }
+      end
+    end
+    name
   end
 end
