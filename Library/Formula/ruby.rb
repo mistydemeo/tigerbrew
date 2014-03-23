a2cb99b26b78de49ea9d5e75818c87156f71bb42
Misty De Meo <mistydemeo@gmail.com>
2014-03-23 14:53:06 -0700

    ruby: fix build on PPC due to __builtin_unreachable
    Fixes #169.

--- Library/Formula/ruby.rb
+++ Library/Formula/ruby.rb
@@ -28,6 +28,11 @@ class Ruby < Formula
     build 2326
   end
 
+  # Removes __builtin_unreachable check from configure, which breaks
+  # the build on PowerPC when using Apple GCC 4.0 or 4.2
+  # Reported upstream: https://bugs.ruby-lang.org/issues/9665
+  patch :DATA if Hardware::CPU.ppc?
+
   def install
     system "autoconf" if build.head?
 
@@ -82,3 +87,87 @@ class Ruby < Formula
     assert_equal 0, $?.exitstatus
   end
 end
+
+__END__
+diff --git a/configure b/configure
+index acc66e6..0fef496 100755
+--- a/configure
++++ b/configure
+@@ -13176,55 +13176,6 @@ $as_echo "$rb_cv_gcc_sync_builtins" >&6; }
+ 
+     fi
+ 
+-    { $as_echo "$as_me:${as_lineno-$LINENO}: checking for __builtin_unreachable" >&5
+-$as_echo_n "checking for __builtin_unreachable... " >&6; }
+-if ${rb_cv_func___builtin_unreachable+:} false; then :
+-  $as_echo_n "(cached) " >&6
+-else
+-  save_CFLAGS="$CFLAGS"
+-CFLAGS="$CFLAGS $rb_cv_warnflags"
+-if test "${ac_c_werror_flag+set}"; then
+-  rb_c_werror_flag="$ac_c_werror_flag"
+-else
+-  unset rb_c_werror_flag
+-fi
+-ac_c_werror_flag=yes
+-cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+-/* end confdefs.h.  */
+-#include <stdlib.h>
+-int
+-main ()
+-{
+-exit(0); __builtin_unreachable();
+-  ;
+-  return 0;
+-}
+-_ACEOF
+-if ac_fn_c_try_link "$LINENO"; then :
+-  rb_cv_func___builtin_unreachable=yes
+-else
+-  rb_cv_func___builtin_unreachable=no
+-fi
+-rm -f core conftest.err conftest.$ac_objext \
+-    conftest$ac_exeext conftest.$ac_ext
+-
+-CFLAGS="$save_CFLAGS"
+-save_CFLAGS=
+-if test "${rb_c_werror_flag+set}"; then
+-  ac_c_werror_flag="$rb_c_werror_flag"
+-else
+-  unset ac_c_werror_flag
+-fi
+-
+-fi
+-{ $as_echo "$as_me:${as_lineno-$LINENO}: result: $rb_cv_func___builtin_unreachable" >&5
+-$as_echo "$rb_cv_func___builtin_unreachable" >&6; }
+-    if test "$rb_cv_func___builtin_unreachable" = yes; then
+-	cat >>confdefs.h <<_ACEOF
+-#define UNREACHABLE __builtin_unreachable()
+-_ACEOF
+-
+-    fi
+ fi
+ 
+ { $as_echo "$as_me:${as_lineno-$LINENO}: checking for exported function attribute" >&5
+diff --git a/configure.in b/configure.in
+index 17ed3ed..aec1838 100644
+--- a/configure.in
++++ b/configure.in
+@@ -1546,17 +1546,6 @@ if test "$GCC" = yes; then
+ 	AC_DEFINE(HAVE_GCC_SYNC_BUILTINS)
+     fi
+ 
+-    AC_CACHE_CHECK(for __builtin_unreachable, rb_cv_func___builtin_unreachable,
+-    [RUBY_WERROR_FLAG(
+-    [AC_TRY_LINK([@%:@include <stdlib.h>],
+-	[exit(0); __builtin_unreachable();],
+-	[rb_cv_func___builtin_unreachable=yes],
+-	[rb_cv_func___builtin_unreachable=no])
+-    ])
+-    ])
+-    if test "$rb_cv_func___builtin_unreachable" = yes; then
+-	AC_DEFINE_UNQUOTED(UNREACHABLE, [__builtin_unreachable()])
+-    fi
+ fi
+ 
+ AC_CACHE_CHECK(for exported function attribute, rb_cv_func_exported, [
