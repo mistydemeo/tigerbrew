513ed62d031276d8dfe9f7d335ab649fbeee7ef3
Misty De Meo <mistydemeo@gmail.com>
2014-03-24 12:13:51 -0700

    heroku-toolbelt: use Tigerbrew-provided ruby
    Fixes #180.

--- Library/Formula/heroku-toolbelt.rb
+++ Library/Formula/heroku-toolbelt.rb
@@ -5,18 +5,17 @@ class HerokuToolbelt < Formula
   url 'http://assets.heroku.com.s3.amazonaws.com/heroku-client/heroku-client-3.3.0.tgz'
   sha1 '5c4760414623b3e92bb0deaf5d49da695f8c7ad4'
 
+  # Not compatible with ruby pre-1.9.1 or so, due to requiring
+  # securerandom and json in the stdlib
+  depends_on 'ruby'
+
   def install
+    inreplace 'bin/heroku', '/usr/bin/env ruby', Formula['ruby'].opt_bin/'ruby'
     libexec.install Dir["*"]
     bin.write_exec_script libexec/"bin/heroku"
   end
 
   test do
     system "#{bin}/heroku", "version"
-  end
-
-  # Possibly temporary; see https://github.com/heroku/heroku/issues/1020
-  def caveats; <<-EOS.undent
-    heroku-toolbelt requires an installation of Ruby 1.9 or greater.
-    EOS
   end
 end
