c3b05e393f21639eb2bf16607238c5f7264a3bd9
Misty De Meo <mistydemeo@gmail.com>
2014-08-18 14:15:17 -0700

    tenfourfox 31.0.0

--- Library/Formula/tenfourfox.rb
+++ Library/Formula/tenfourfox.rb
@@ -3,23 +3,23 @@ require 'formula'
 class Tenfourfox < Formula
   homepage 'http://www.floodgap.com/software/tenfourfox/'
   urlbase = 'https://downloads.sourceforge.net/tenfourfox/'
-  version '24.5.0'
+  version '31.0.0'
 
-  depends_on :arch => :ppc
+  # depends_on :arch => :ppc
 
   urls = {
-    :g3 => ['G3', '52164ae84ae5dde0f4b6876b75965d04b6667010'],
-    :g4 => ['7400', 'd151431f3fb9836669efc89be2dacdd030e98157'],
-    :g4e => ['7450', '4bd546f39c016ff453e9183b6e2485c5e72b11b2'],
+    :g3 => ['G3', '6443902f14010ad87c75014efb7f9573ffe6799b'],
+    :g4 => ['7400', '9d8e6b6e7701fde539fa01f5f3a5ec79bf7b753d'],
+    :g4e => ['7450', '831a6df1290edaf27faad06e9070e40ba065deaa'],
     # There aren't separate 32/64-bit binaries
-    :g5 => ['G5', '6e2ed867bf8b8bea9d71244772bc8e3463cc245a'],
-    :g5_64 => ['G5', '6e2ed867bf8b8bea9d71244772bc8e3463cc245a']
+    :g5 => ['G5', '6881dd3295a544ca9e62c44bdf42555b0e6beaed'],
+    :g5_64 => ['G5', '6881dd3295a544ca9e62c44bdf42555b0e6beaed']
   }
 
   # Formula needs a URL and sha1 even if it can't be installed
   prefix, sh1 = urls[Hardware::CPU.family] || urls[:g3]
 
-  url urlbase+version+'/TenFourFox'+prefix+'-'+version+'.app.zip'
+  url urlbase+version+'/TenFourFox'+prefix+'-31.0.app.zip'
   sha1 sh1
 
   def install
