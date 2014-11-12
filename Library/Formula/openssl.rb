02ddd490a0f35e3700e886cfd21b30a65dac4897
Misty De Meo <mistydemeo@gmail.com>
2014-11-11 23:24:08 -0800

    openssl: use sha1 for Tiger

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -4,7 +4,7 @@ class Openssl < Formula
   homepage "https://openssl.org"
   url "https://www.openssl.org/source/openssl-1.0.1j.tar.gz"
   mirror "https://raw.githubusercontent.com/DomT4/LibreMirror/master/OpenSSL/openssl-1.0.1j.tar.gz"
-  sha256 "1b60ca8789ba6f03e8ef20da2293b8dc131c39d83814e775069f02d26354edf3"
+  sha1 "cff86857507624f0ad42d922bb6f77c4f1c2b819"
 
   bottle do
   end
