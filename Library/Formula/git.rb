ed4ffb0e4853ef47908cc074cf7921eb45b52cd5
chdiza <chdiza@gmail.com>
2014-12-11 18:49:20 -0500

    git: Bump to 2.2.0 and change to double-quotes
    The latter will make it easier to compare Tigerbrew's git formula
    to Homebrew's.

--- Library/Formula/git.rb
+++ Library/Formula/git.rb
@@ -1,9 +1,9 @@
-require 'formula'
+require "formula"
 
 class Git < Formula
   homepage "http://git-scm.com"
-  url "https://www.kernel.org/pub/software/scm/git/git-2.1.2.tar.gz"
-  sha1 "1385edbc9aa77b6e0971f52f75ba4f2ff35c1aa2"
+  url "https://www.kernel.org/pub/software/scm/git/git-2.2.0.tar.gz"
+  sha1 "b59ad3f9e9c933960b064e31d793757c145be83a"
 
   head "https://github.com/git/git.git", :shallow => false
 
@@ -15,41 +15,41 @@ class Git < Formula
   end
 
   resource "man" do
-    url "https://www.kernel.org/pub/software/scm/git/git-manpages-2.1.2.tar.gz"
-    sha1 "912ff9865d6c6ab3d411566a6805431c7d653efd"
+    url "https://www.kernel.org/pub/software/scm/git/git-manpages-2.2.0.tar.gz"
+    sha1 "6d068b6526ef035f48505894de7452df22e6ed40"
   end
 
   resource "html" do
-    url "https://www.kernel.org/pub/software/scm/git/git-htmldocs-2.1.2.tar.gz"
-    sha1 "229b697db2c6f7ba0831842849f9dc0651f0fc63"
+    url "https://www.kernel.org/pub/software/scm/git/git-htmldocs-2.2.0.tar.gz"
+    sha1 "fef0a1944d9d94d5a97e2dae3e53b9818af7973d"
   end
 
-  option 'with-blk-sha1', 'Compile with the block-optimized SHA1 implementation'
-  option 'without-completions', 'Disable bash/zsh completions from "contrib" directory'
-  option 'with-brewed-openssl', "Build with Homebrew OpenSSL instead of the system version" if MacOS.version > :snow_leopard
-  option 'with-brewed-curl', "Use Homebrew's version of cURL library" if MacOS.version > :leopard
-  option 'with-brewed-svn', "Use Homebrew's version of SVN"
-  option 'with-persistent-https', 'Build git-remote-persistent-https from "contrib" directory'
+  option "with-blk-sha1", "Compile with the block-optimized SHA1 implementation"
+  option "without-completions", "Disable bash/zsh completions from 'contrib' directory"
+  option "with-brewed-openssl", "Build with Homebrew OpenSSL instead of the system version" if MacOS.version > :snow_leopard
+  option "with-brewed-curl", "Use Homebrew's version of cURL library" if MacOS.version > :leopard
+  option "with-brewed-svn", "Use Homebrew's version of SVN"
+  option "with-persistent-https", "Build git-remote-persistent-https from 'contrib' directory"
 
   if MacOS.version == :tiger
     # system tar has odd permissions errors
-    depends_on 'gnu-tar' => :build
+    depends_on "gnu-tar" => :build
     # Tiger's ld produces bad install-names for a keg-only curl
-    depends_on 'ld64' => :build
-    depends_on 'cctools' => :build
+    depends_on "ld64" => :build
+    depends_on "cctools" => :build
   end
 
   if MacOS.version < :snow_leopard
-    depends_on 'curl'
+    depends_on "curl"
   else
-    depends_on 'curl' if build.with? 'brewed-curl'
+    depends_on "curl" if build.with? "brewed-curl"
   end
   depends_on :expat
-  depends_on 'pcre' => :optional
-  depends_on 'gettext' => :optional
-  depends_on 'openssl' if MacOS.version < :leopard || build.with?('brewed-openssl')
-  depends_on 'go' => :build if build.with? 'persistent-https'
-  depends_on 'subversion' => 'perl' if build.with? 'brewed-svn'
+  depends_on "pcre" => :optional
+  depends_on "gettext" => :optional
+  depends_on "openssl" if MacOS.version < :leopard || build.with?("brewed-openssl")
+  depends_on "go" => :build if build.with? "persistent-https"
+  depends_on "subversion" => "perl" if build.with? "brewed-svn"
 
   # ld64 understands -rpath but rejects it on Tiger
   patch :p1 do
@@ -69,27 +69,27 @@ class Git < Formula
     end
 
     if MacOS.version == :tiger
-      tar = Formula['gnu-tar']
+      tar = Formula["gnu-tar"]
       tab = Tab.for_keg tar.installed_prefix
-      tar_name = tab.used_options.include?('--default-names') ? tar.bin/'tar' : tar.bin/'gtar'
-      inreplace 'Makefile' do |s|
-        s.change_make_var! 'TAR', tar_name.to_s
+      tar_name = tab.used_options.include?("--default-names") ? tar.bin/"tar" : tar.bin/"gtar"
+      inreplace "Makefile" do |s|
+        s.change_make_var! "TAR", tar_name.to_s
       end
     end
 
     # If these things are installed, tell Git build system to not use them
-    ENV['NO_FINK'] = '1'
-    ENV['NO_DARWIN_PORTS'] = '1'
-    ENV['V'] = '1' # build verbosely
-    ENV['NO_R_TO_GCC_LINKER'] = '1' # pass arguments to LD correctly
-    ENV['PYTHON_PATH'] = which 'python'
-    ENV['PERL_PATH'] = which 'perl'
-    ENV['CURLDIR'] = Formula['curl'].opt_prefix if MacOS.version < :snow_leopard
-    ENV['NO_APPLE_COMMON_CRYPTO'] = '1' if MacOS.version < :leopard
+    ENV["NO_FINK"] = "1"
+    ENV["NO_DARWIN_PORTS"] = "1"
+    ENV["V"] = "1" # build verbosely
+    ENV["NO_R_TO_GCC_LINKER"] = "1" # pass arguments to LD correctly
+    ENV["PYTHON_PATH"] = which "python"
+    ENV["PERL_PATH"] = which "perl"
+    ENV["CURLDIR"] = Formula["curl"].opt_prefix if MacOS.version < :snow_leopard
+    ENV["NO_APPLE_COMMON_CRYPTO"] = "1" if MacOS.version < :leopard
 
     perl_version = /\d\.\d+/.match(`perl --version`)
 
-    if build.with? 'brewed-svn'
+    if build.with? "brewed-svn"
       ENV["PERLLIB_EXTRA"] = "#{Formula["subversion"].prefix}/Library/Perl/#{perl_version}/darwin-thread-multi-2level"
     elsif MacOS.version >= :mavericks
       ENV["PERLLIB_EXTRA"] = %W{
@@ -101,20 +101,20 @@ class Git < Formula
       }.join(":")
     end
 
-    unless quiet_system ENV['PERL_PATH'], '-e', 'use ExtUtils::MakeMaker'
-      ENV['NO_PERL_MAKEMAKER'] = '1'
+    unless quiet_system ENV["PERL_PATH"], "-e", "use ExtUtils::MakeMaker"
+      ENV["NO_PERL_MAKEMAKER"] = "1"
     end
 
-    ENV['BLK_SHA1'] = '1' if build.with? 'blk-sha1'
+    ENV["BLK_SHA1"] = "1" if build.with? "blk-sha1"
 
-    if build.with? 'pcre'
-      ENV['USE_LIBPCRE'] = '1'
-      ENV['LIBPCREDIR'] = Formula['pcre'].opt_prefix
+    if build.with? "pcre"
+      ENV["USE_LIBPCRE"] = "1"
+      ENV["LIBPCREDIR"] = Formula["pcre"].opt_prefix
     end
 
-    ENV['NO_GETTEXT'] = '1' if build.without? 'gettext'
+    ENV["NO_GETTEXT"] = "1" if build.without? "gettext"
 
-    ENV['GIT_DIR'] = cached_download/".git" if build.head?
+    ENV["GIT_DIR"] = cached_download/".git" if build.head?
 
     system "make", "prefix=#{prefix}",
                    "sysconfdir=#{etc}",
@@ -124,46 +124,46 @@ class Git < Formula
                    "install"
 
     # Install the OS X keychain credential helper
-    cd 'contrib/credential/osxkeychain' do
+    cd "contrib/credential/osxkeychain" do
       system "make", "CC=#{ENV.cc}",
                      "CFLAGS=#{ENV.cflags}",
                      "LDFLAGS=#{ENV.ldflags}"
-      bin.install 'git-credential-osxkeychain'
+      bin.install "git-credential-osxkeychain"
       system "make", "clean"
     end
 
     # Install git-subtree
-    cd 'contrib/subtree' do
+    cd "contrib/subtree" do
       system "make", "CC=#{ENV.cc}",
                      "CFLAGS=#{ENV.cflags}",
                      "LDFLAGS=#{ENV.ldflags}"
-      bin.install 'git-subtree'
+      bin.install "git-subtree"
     end
 
-    if build.with? 'persistent-https'
-      cd 'contrib/persistent-https' do
+    if build.with? "persistent-https"
+      cd "contrib/persistent-https" do
         system "make"
-        bin.install 'git-remote-persistent-http',
-                    'git-remote-persistent-https',
-                    'git-remote-persistent-https--proxy'
+        bin.install "git-remote-persistent-http",
+                    "git-remote-persistent-https",
+                    "git-remote-persistent-https--proxy"
       end
     end
 
-    if build.with? 'completions'
-      # install the completion script first because it is inside 'contrib'
-      bash_completion.install 'contrib/completion/git-completion.bash'
-      bash_completion.install 'contrib/completion/git-prompt.sh'
+    if build.with? "completions"
+      # install the completion script first because it is inside "contrib"
+      bash_completion.install "contrib/completion/git-completion.bash"
+      bash_completion.install "contrib/completion/git-prompt.sh"
 
-      zsh_completion.install 'contrib/completion/git-completion.zsh' => '_git'
+      zsh_completion.install "contrib/completion/git-completion.zsh" => "_git"
       cp "#{bash_completion}/git-completion.bash", zsh_completion
     end
 
-    (share+'git-core').install 'contrib'
+    (share+"git-core").install "contrib"
 
     # We could build the manpages ourselves, but the build process depends
     # on many other packages, and is somewhat crazy, this way is easier.
-    man.install resource('man')
-    (share+'doc/git-doc').install resource('html')
+    man.install resource("man")
+    (share+"doc/git-doc").install resource("html")
 
     # Make html docs world-readable
     chmod 0644, Dir["#{share}/doc/git-doc/**/*.{html,txt}"]
@@ -174,14 +174,14 @@ class Git < Formula
     The OS X keychain credential helper has been installed to:
       #{HOMEBREW_PREFIX}/bin/git-credential-osxkeychain
 
-    The 'contrib' directory has been installed to:
+    The "contrib" directory has been installed to:
       #{HOMEBREW_PREFIX}/share/git-core/contrib
     EOS
   end
 
   test do
     HOMEBREW_REPOSITORY.cd do
-      assert_equal 'bin/brew', `#{bin}/git ls-files -- bin`.strip
+      assert_equal "bin/brew", `#{bin}/git ls-files -- bin`.strip
     end
   end
 end
