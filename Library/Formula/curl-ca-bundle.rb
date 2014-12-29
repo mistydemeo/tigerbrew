a45686aee09a546a1e0f6b4d5553f235b60521f3
Misty De Meo <mistydemeo@gmail.com>
2014-12-29 14:54:16 -0800

    curl-ca-bundle 2014-09-04

--- Library/Formula/curl-ca-bundle.rb
+++ Library/Formula/curl-ca-bundle.rb
@@ -1,7 +1,9 @@
 class CurlCaBundle < Formula
   homepage "http://curl.haxx.se/docs/caextract.html"
-  url "https://downloads.sourceforge.net/project/tigerbrew/curl-ca-bundle-1.87.tar.bz2"
-  sha256 "41742f0c6aa689543ad037d1f7615b8620dae399d3cf2061a8d86d84a1b41f7f"
+  url "https://raw.githubusercontent.com/bagder/ca-bundle/b072e4efd42b8602855bba127586c42fcf4f73d5/ca-bundle.crt",
+    :using => :nounzip
+  sha256 "8c7a422750d1ff035b940bb74c13fdea2d1dc55cda1eafb68201088ed4b47d35"
+  version "2014-09-04"
 
   def install
     share.install "ca-bundle.crt"
