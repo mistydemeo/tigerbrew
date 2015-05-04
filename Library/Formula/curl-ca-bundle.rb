df59afd6bd3abfb8f98f071de3f09aae2f3a695c
Misty De Meo <mistydemeo@gmail.com>
2015-05-04 13:45:13 -0700

    curl-ca-bundle: add 2014-09-04 bottle

--- Library/Formula/curl-ca-bundle.rb
+++ Library/Formula/curl-ca-bundle.rb
@@ -5,6 +5,12 @@ class CurlCaBundle < Formula
   sha256 "8c7a422750d1ff035b940bb74c13fdea2d1dc55cda1eafb68201088ed4b47d35"
   version "2014-09-04"
 
+  bottle do
+    cellar :any
+    sha1 "1a2ddcf4287a970f3951eac1654d47c05e12c330" => :leopard_g3
+    sha1 "34afbe9099fecb7e01bb345490b42795386555db" => :leopard_altivec
+  end
+
   def install
     share.install "ca-bundle.crt"
   end
