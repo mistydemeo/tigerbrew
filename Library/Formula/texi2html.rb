a2c6a7fa61c71f20cfe8c1b6ff65b156d88a139c
Misty De Meo <mistydemeo@gmail.com>
2015-05-07 12:35:14 -0700

    texi2html: add Leopard bottles

--- Library/Formula/texi2html.rb
+++ Library/Formula/texi2html.rb
@@ -7,6 +7,8 @@ class Texi2html < Formula
 
   bottle do
     sha1 "dab75b6b742681e2808178d0c2ea659ac20ccf19" => :tiger_altivec
+    sha1 "58cb2e4901212c31394dfdf317d1394a365eb98c" => :leopard_g3
+    sha1 "59304169ba3f1d720f2e0db52f41204a0d275e5e" => :leopard_altivec
   end
 
   keg_only :provided_pre_mountain_lion
