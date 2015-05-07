c620bf93a060ac75977cd88107fdfa0a34b988b0
Misty De Meo <mistydemeo@gmail.com>
2015-05-07 09:42:34 -0700

    faad2: add Leopard bottles

--- Library/Formula/faad2.rb
+++ Library/Formula/faad2.rb
@@ -11,6 +11,8 @@ class Faad2 < Formula
   bottle do
     cellar :any
     sha1 "96262445fa48886dc6ee84308b8b6118b7b796dc" => :tiger_altivec
+    sha1 "025ba2eeae000895a18647ac2e1560f620dffa81" => :leopard_g3
+    sha1 "3115d2defb838d13093d8a5401266178fc6fd6fe" => :leopard_altivec
   end
 
   def install
