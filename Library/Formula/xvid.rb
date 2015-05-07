e335a03f89c5607ae0be9bcff68a2a973b6ef665
Misty De Meo <mistydemeo@gmail.com>
2015-05-06 20:44:03 -0700

    xvid: add Tiger bottle

--- Library/Formula/xvid.rb
+++ Library/Formula/xvid.rb
@@ -7,10 +7,7 @@ class Xvid < Formula
 
   bottle do
     cellar :any
-    revision 2
-    sha256 "c152517b75984c64ed0bf4e37a383feb7b88aee70a8d3249b4f9d7ea1e4314be" => :yosemite
-    sha256 "65f9ccd09973b890288f77aa0c01c01587e0a5209a3b63f2b237de2a942f95e1" => :mavericks
-    sha256 "d2b3dfcc3c0f53b5dbc530b4ddfc67227b0a7c0520bb0a01f7d6922c39c47dca" => :mountain_lion
+    sha1 "7bafed6dce60e1478fd80421e741230beb70611d" => :tiger_altivec
   end
 
   def install
