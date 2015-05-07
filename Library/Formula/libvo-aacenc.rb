021de933f00d8819dacf70c449050240415eac01
Misty De Meo <mistydemeo@gmail.com>
2015-05-06 20:35:39 -0700

    libvo-aacenc: add Tiger bottle

--- Library/Formula/libvo-aacenc.rb
+++ Library/Formula/libvo-aacenc.rb
@@ -5,9 +5,7 @@ class LibvoAacenc < Formula
 
   bottle do
     cellar :any
-    sha256 "cf63ddcb79e40b79264507393ed4fa1b223feecf4638f0e58fef464db722b554" => :yosemite
-    sha256 "645f4294e8512add5c5f263cd8273c93e22eab565307ada5f8804ef7b9d41d8d" => :mavericks
-    sha256 "1a2fc17d25d424a4385d2c792b4e09ff0409b5c264cc03aec2182e40dc21295a" => :mountain_lion
+    sha1 "db988fca9280de51d16bddcf04bd22e9b3422b58" => :tiger_altivec
   end
 
   def install
