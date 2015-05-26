27de50eda430495f60d3e30414c165eaace57be5
Misty De Meo <mistydemeo@gmail.com>
2015-05-26 14:34:39 -0700

    mosh: fix timestamps on PPC
    Fixes #87.

--- Library/Formula/mobile-shell.rb
+++ Library/Formula/mobile-shell.rb
@@ -19,6 +19,12 @@ class MobileShell < Formula
   depends_on "pkg-config" => :build
   depends_on "protobuf"
 
+  # Fix timestamp code on PPC; will be in next release
+  patch do
+    url "https://github.com/keithw/mosh/commit/ba47f9f75081243406b99eeb2303f775cca7a438.patch"
+    sha256 "843caa7c1de7b0d05b0298050803758c56b2447843de1be27b6a48be9a1fa8ae"
+  end
+
   def install
     system "./autogen.sh" if build.head?
 
