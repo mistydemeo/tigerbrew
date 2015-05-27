032a03e86e721d87f687207a2239bd40d44562bd
Misty De Meo <mistydemeo@gmail.com>
2015-05-27 16:59:25 -0700

    isl: remove invalid bottle block

--- Library/Formula/isl.rb
+++ Library/Formula/isl.rb
@@ -11,9 +11,6 @@ class Isl < Formula
 
   bottle do
     cellar :any
-    sha1 "b75c971058946c3a261a7cb807fc032644b8337b" => :tiger_altivec
-    sha1 "13905e3263d3b3218aaa590bb767a0de5653ff14" => :leopard_g3
-    sha1 "5a48c443a0a3a67335843346217138073fdf074c" => :leopard_altivec
   end
 
   head do
