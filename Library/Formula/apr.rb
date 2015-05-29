9ca85b22441f62ccdc6c05a5c7cf90ab79268ac7
Misty De Meo <mistydemeo@gmail.com>
2015-05-29 13:34:11 -0700

    apr: add Leopard bottles

--- Library/Formula/apr.rb
+++ Library/Formula/apr.rb
@@ -4,9 +4,8 @@ class Apr < Formula
   sha256 "7d03ed29c22a7152be45b8e50431063736df9e1daa1ddf93f6a547ba7a28f67a"
 
   bottle do
-    sha256 "5ae775d6f6a3fb46b1b262e1c0ecb2aca4e5a29ea1f841672ac430b4d49606af" => :yosemite
-    sha256 "691b67fdf7077bca9db5cb0fcc40856dd82914bb00868c4e1a6a692abca87913" => :mavericks
-    sha256 "4d5e7c20d507c48712d8ba549f500e7e58a4782dd1e88aee6d2eb1a169aeeaf9" => :mountain_lion
+    sha256 "3ae92c725f0c8ad9aa5b1044b332b76eeecc4a9831d41450c55bdd17577b626e" => :leopard_g3
+    sha256 "0720fdf5a6be0810571ed9b03a67b8fef9e730d6bd1889ffc7e6e3dcbcbc3130" => :leopard_altivec
   end
 
   keg_only :provided_by_osx, "Apple's CLT package contains apr."
