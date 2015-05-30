a6c9a69eb6a80669766c72eaa326ad00e024e740
Misty De Meo <mistydemeo@gmail.com>
2015-05-29 19:50:20 -0700

    apr-util: add Tiger bottle

--- Library/Formula/apr-util.rb
+++ Library/Formula/apr-util.rb
@@ -5,6 +5,7 @@ class AprUtil < Formula
   revision 1
 
   bottle do
+    sha256 "4f38f40032b9d1583249614223acfc0520e7000e8804dfedd2e0ad8b0f20defc" => :tiger_altivec
     sha256 "00de6007379bd4246825cdabc7c4eea0da4e5d2c97a545977fbe01782997869f" => :leopard_g3
     sha256 "e1f4b891239cef8a1eda4a7e22d36e3dbe8648b8db162e6a6d210874aa0c3957" => :leopard_altivec
   end
