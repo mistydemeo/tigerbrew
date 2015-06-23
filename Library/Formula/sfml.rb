569f2ee81513f772d4934261aebc525e2b664d9f
Misty De Meo <mistydemeo@gmail.com>
2015-06-22 17:39:18 -0700

    sfml: use 2.1 for 10.5 and 10.6
    Fixes #337.

--- Library/Formula/sfml.rb
+++ Library/Formula/sfml.rb
@@ -2,8 +2,16 @@ class Sfml < Formula
   # Don't update SFML until there's a corresponding CSFML release
   desc "Multi-media library with bindings for multiple languages"
   homepage "http://www.sfml-dev.org/"
-  url "http://www.sfml-dev.org/files/SFML-2.3-sources.zip"
-  sha256 "a1dc8b00958000628c5394bc8438ba1aa5971fbeeef91a2cf3fa3fff443de7c1"
+
+  # SFML 2.2+ require Lion or newer
+  case MacOS.version
+  when 10.5, 10.6
+    url "http://www.sfml-dev.org/download/sfml/2.1/SFML-2.1-sources.zip"
+    sha256 "5f46d7748223be3f0c6a9fcf18c0016d227f7b1903cdbcd85f61ddbc82ef95bf"
+  else
+    url "http://www.sfml-dev.org/files/SFML-2.3-sources.zip"
+    sha256 "a1dc8b00958000628c5394bc8438ba1aa5971fbeeef91a2cf3fa3fff443de7c1"
+  end
 
   head "https://github.com/SFML/SFML.git"
 
@@ -14,6 +22,9 @@ class Sfml < Formula
     sha256 "1dd68903fa88cf39d3d12d9380d0bfb9c10585d2970b2cca24597e8c3604acf8" => :mountain_lion
   end
 
+  # SFML 2.x requires 10.5; it appears to be a substantial rewrite from 1.x,
+  # so don't bother trying to support SFML 1.6 for 10.4
+  depends_on :macos => :leopard
   depends_on "cmake" => :build
   depends_on "doxygen" => :optional
   depends_on "flac"
@@ -21,6 +32,7 @@ class Sfml < Formula
   depends_on "glew"
   depends_on "jpeg"
   depends_on "libogg"
+  depends_on "libsndfile"
   depends_on "libvorbis"
   depends_on "openal-soft"
 
@@ -32,6 +44,15 @@ class Sfml < Formula
     # (https://github.com/Homebrew/homebrew/pull/35279) but leave the
     # headers that were moved there in https://github.com/SFML/SFML/pull/795
     rm_rf Dir["extlibs/*"] - ["extlibs/headers"]
+
+    if MacOS.version < :lion
+      # SFML tries to install contents from extlibs, even if they weren't
+      # the actual copies linked against in the build
+      inreplace "CMakeLists.txt", /install\(DIRECTORY extlibs\/libs-osx.+$/, ""
+
+      # SFML 2.1 doesn't properly add a link flag for openal-soft
+      ENV.append "LDFLAGS", "-lopenal"
+    end
 
     system "cmake", ".", *args
     system "make", "install"
