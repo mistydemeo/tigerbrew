fd2f29027ced1fa75bc4e35a6456d640bdf3dbfe
Toby <toby@telegraphics.com.au>
2015-06-27 11:25:31 -0400

    Update CA bundle to 2015-04-27 version.

--- Library/Formula/curl-ca-bundle.rb
+++ Library/Formula/curl-ca-bundle.rb
@@ -1,9 +1,9 @@
 class CurlCaBundle < Formula
   homepage "http://curl.haxx.se/docs/caextract.html"
-  url "https://raw.githubusercontent.com/bagder/ca-bundle/b072e4efd42b8602855bba127586c42fcf4f73d5/ca-bundle.crt",
+  url "https://raw.githubusercontent.com/bagder/ca-bundle/bff056d04b9e2c92ea8c83b2e39be9c8d0501039/ca-bundle.crt",
     :using => :nounzip
-  sha256 "8c7a422750d1ff035b940bb74c13fdea2d1dc55cda1eafb68201088ed4b47d35"
-  version "2014-09-04"
+  sha256 "0f119da204025da7808273fab42ed8e030cafb5c7ea4e1deda4e75f066f528fb"
+  version "2015-04-27"
 
   bottle do
     cellar :any
