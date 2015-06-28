bfe019ac4d5b63633c4ed3c78fde6cd576a1f9c4
Misty De Meo <mistydemeo@gmail.com>
2015-06-27 20:25:21 -0700

    curl-ca-bundle: add bottles

--- Library/Formula/curl-ca-bundle.rb
+++ Library/Formula/curl-ca-bundle.rb
@@ -7,8 +7,10 @@ class CurlCaBundle < Formula
 
   bottle do
     cellar :any
-    sha1 "1a2ddcf4287a970f3951eac1654d47c05e12c330" => :leopard_g3
-    sha1 "34afbe9099fecb7e01bb345490b42795386555db" => :leopard_altivec
+    sha256 "2ca60a8bbb648ded824dce45eb150fd0e9d32f8cbd983945db7bee4c6e58b1ea" => :tiger_g3
+    sha256 "fba559a40c232629dade3aaa6c13206098d6428fc5716ed05002ed748c60f946" => :tiger_altivec
+    sha256 "3ec0182385448739a37805ad3f1561ec44706506cb9fa15cd8573023c4e8fb0c" => :leopard_g3
+    sha256 "b168b3d05821dd315a7a8665f63f6a327957f129dda209f20df9aaad45f0742d" => :leopard_altivec
   end
 
   def install
