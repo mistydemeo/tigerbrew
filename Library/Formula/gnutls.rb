ec67e3d49ef005ee92727e6a674d8f84e253a633
Misty De Meo <mistydemeo@gmail.com>
2015-08-19 11:55:53 -0700

    gnutls: add Leopard bottles

--- Library/Formula/gnutls.rb
+++ Library/Formula/gnutls.rb
@@ -10,6 +10,8 @@ class Gnutls < Formula
 
   bottle do
     cellar :any
+    sha256 "eaab9415a720ad25143b1f28555d393faa0538547576219d7765a3f2bb12a81e" => :leopard_g3
+    sha256 "79aadee18c97f426c153af19386294307b91b434ed478fe3bfb9fecc99360824" => :leopard_altivec
   end
 
   depends_on "pkg-config" => :build
