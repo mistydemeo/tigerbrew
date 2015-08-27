f7ecfce562f9c9262150616380ea50c857e7d625
Misty De Meo <mistydemeo@gmail.com>
2015-08-27 10:52:57 -0700

    superenv: use HOMEBREW_REPOSITORY, not HOMEBREW_PREFIX
    Fixes #349.

--- Library/Homebrew/extend/ENV/super.rb
+++ Library/Homebrew/extend/ENV/super.rb
@@ -49,6 +49,7 @@ module Superenv
     self["HOMEBREW_BREW_FILE"] = HOMEBREW_BREW_FILE.to_s
     self["HOMEBREW_PREFIX"] = HOMEBREW_PREFIX.to_s
     self["HOMEBREW_CELLAR"] = HOMEBREW_CELLAR.to_s
+    self["HOMEBREW_REPOSITORY"] = HOMEBREW_REPOSITORY.to_s
     self["HOMEBREW_TEMP"] = HOMEBREW_TEMP.to_s
     self["HOMEBREW_SDKROOT"] = effective_sysroot
     self["HOMEBREW_OPTFLAGS"] = determine_optflags
