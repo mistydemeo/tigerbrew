b9ce93bb10341d083b8689b3b039f01305a3b3dd
Misty De Meo <mistydemeo@gmail.com>
2015-09-29 08:23:36 -0700

    gettext: update 0.19.6 bottles

--- Library/Formula/gettext.rb
+++ Library/Formula/gettext.rb
@@ -6,6 +6,9 @@ class Gettext < Formula
   sha256 "9b95816620fd1168cb4eeca0e9dc0ffd86e864fc668f76f5e37cc054d6982e51"
 
   bottle do
+    sha256 "41a26cf4db47c63a8d1b93092dc45bb56a832701d11838277221fd934af4ed8a" => :tiger_altivec
+    sha256 "5c7b69d10f0a5e330285d1806d10ce85f0a8e01eb727ba92bcdb815d2e08f6ca" => :leopard_g3
+    sha256 "522195faf5f9c5cbd80ecfb7e6f72653a248cc893be53a931c2949645f48f51b" => :leopard_altivec
   end
 
   keg_only :shadowed_by_osx, "OS X provides the BSD gettext library and some software gets confused if both are in the library path."
