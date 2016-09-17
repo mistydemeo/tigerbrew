ab666acbba60c350be8eac6f6b32b57cc07d08f9
ilovezfs <ilovezfs@icloud.com>
2016-09-17 06:01:28 -0700

    dwarf: revision for readline

--- Library/Formula/dwarf.rb
+++ Library/Formula/dwarf.rb
@@ -3,6 +3,7 @@ class Dwarf < Formula
   homepage "https://code.google.com/p/dwarf-ng/"
   url "https://dwarf-ng.googlecode.com/files/dwarf-0.3.0.tar.gz"
   sha256 "85062d0d3e8aa31374dd085cb79ce02c2b8737e9b143f640a262556233715763"
+  revision 1
 
   depends_on "readline"
 
