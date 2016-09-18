fe4e29c119d13c189182ec4fa3ebf29abe90ca7f
ilovezfs <ilovezfs@icloud.com>
2016-09-18 03:17:43 -0700

    gqlplus: revision for readline

--- Library/Formula/gqlplus.rb
+++ Library/Formula/gqlplus.rb
@@ -3,6 +3,7 @@ class Gqlplus < Formula
   homepage "http://gqlplus.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/gqlplus/gqlplus/1.15/gqlplus-1.15.tar.gz"
   sha256 "9a539cdcf952b4acd2ae2d940772366bf6c9ee0fb51846c02d3c7dc1df3056d5"
+  revision 1
 
   depends_on "readline"
 
