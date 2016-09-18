875f82031df127ddd6e550bd3c6712c6c58ecf4b
ilovezfs <ilovezfs@icloud.com>
2016-09-18 05:53:32 -0700

    ldapvi: revision for readline

--- Library/Formula/ldapvi.rb
+++ Library/Formula/ldapvi.rb
@@ -4,14 +4,14 @@ class Ldapvi < Formula
   url "http://www.lichteblau.com/download/ldapvi-1.7.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/l/ldapvi/ldapvi_1.7.orig.tar.gz"
   sha256 "6f62e92d20ff2ac0d06125024a914b8622e5b8a0a0c2d390bf3e7990cbd2e153"
+  revision 2
+
   bottle do
     cellar :any
     sha1 "c87e30a0955bb200d8f88011ee8eec8291562b93" => :yosemite
     sha1 "f3bd57a8d0ce4f7e19ade3cf051396169441bdcf" => :mavericks
     sha1 "13d1720d60f6557a57214baa7622f7ffac889edc" => :mountain_lion
   end
-
-  revision 1
 
   depends_on "pkg-config" => :build
   depends_on "gettext"
