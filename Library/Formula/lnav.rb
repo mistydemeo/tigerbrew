a5ffc1a18484c058c586ef6566193434afdc1dbc
ilovezfs <ilovezfs@icloud.com>
2016-09-18 09:27:16 -0700

    lnav: revision for readline

--- Library/Formula/lnav.rb
+++ Library/Formula/lnav.rb
@@ -3,6 +3,7 @@ class Lnav < Formula
   homepage "http://lnav.org"
   url "https://github.com/tstack/lnav/releases/download/v0.7.3/lnav-0.7.3.tar.gz"
   sha256 "7eb052a8dc60bef6c5f4a8e7135b7089b4413edbf156cc8bafce0557d3166df3"
+  revision 1
 
   bottle do
     sha256 "078a22f68115ff5d271f4cd530d93ef37e3b999b993843a34f34e2f65a6e8ece" => :el_capitan
