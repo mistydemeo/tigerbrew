97f4f7ada9d6b278680769656956dffb29969afb
ilovezfs <ilovezfs@icloud.com>
2016-09-18 09:37:34 -0700

    pdsh: revision for readline

--- Library/Formula/pdsh.rb
+++ Library/Formula/pdsh.rb
@@ -4,6 +4,7 @@ class Pdsh < Formula
   url "https://code.google.com/p/pdsh.git",
       :tag => "pdsh-2.31",
       :revision => "e1c8e71dd6a26b40cd067a8322bd14e10e4f7ded"
+  revision 1
 
   head "https://code.google.com/p/pdsh.git"
 
