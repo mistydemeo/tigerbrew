7bdfa550f8b5dd38afd3f106fb99fa79571618be
ilovezfs <ilovezfs@icloud.com>
2016-09-18 09:35:26 -0700

    nickle: revision for readline

--- Library/Formula/nickle.rb
+++ Library/Formula/nickle.rb
@@ -3,6 +3,7 @@ class Nickle < Formula
   homepage "http://www.nickle.org/"
   url "http://www.nickle.org/release/nickle-2.77.tar.gz"
   sha256 "a35e7ac9a3aa41625034db5c809effc208edd2af6a4adf3f4776fe60d9911166"
+  revision 1
 
   depends_on "readline"
 
