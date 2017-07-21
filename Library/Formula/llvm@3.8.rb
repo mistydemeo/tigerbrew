49c54369b1e09978d8b7da6ba1c33c2fef2a815e
Misty De Meo <mistydemeo@gmail.com>
2017-07-20 20:31:59 -0700

    llvm@3.8: fix passing block argument

--- Library/Formula/llvm@3.8.rb
+++ Library/Formula/llvm@3.8.rb
@@ -346,7 +346,7 @@ class LlvmAT38 < Formula
     # HUGE HACK
     # These patches cover several subprojects, so they can't use Homebrew's normal
     # patching mechanism.
-    resources.select { |r| r.url.end_with?(".diff") || r.url.end_with?(".patch") }.each do
+    resources.select { |r| r.url.end_with?(".diff") || r.url.end_with?(".patch") }.each do |res|
       buildpath.install res
       filename = res.name
       system "patch", "-p1", filename
