e599e661a8e4f2af68e2a4d8f6d605cfcc7f2fb8
Misty De Meo <mistydemeo@gmail.com>
2018-01-01 22:23:52 +1100

    mysql: fix download URL
    Fixes #546.

--- Library/Formula/mysql.rb
+++ Library/Formula/mysql.rb
@@ -1,7 +1,7 @@
 class Mysql < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/5.6/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.35.tar.gz"
+  url "https://cdn.mysql.com/archives/mysql-5.6/mysql-5.6.35.tar.gz"
   sha256 "dddcba169b98844d7c65346cbd791c853edf942d78440381685087b84aa35020"
 
   bottle do
