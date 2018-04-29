edf2aa9573c2ffb9ae16ce7ed0b41876621cb1d0
Misty De Meo <mistydemeo@gmail.com>
2018-04-28 19:55:29 -0700

    pcre2: add bottles

--- Library/Formula/pcre2.rb
+++ Library/Formula/pcre2.rb
@@ -8,10 +8,9 @@ class Pcre2 < Formula
 
   bottle do
     cellar :any
-    sha256 "df6f0855251cd664b41896e72262d28ed73b82b585b09d5df1a8d54783c8583c" => :el_capitan
-    sha256 "acd343182f0033d61a8b9266909a1c3a609d9a450d1cf28fe30fefa9c54c36e3" => :yosemite
-    sha256 "febc1cf22e5da7f4f873dadda0d88a158602d99b49d61ed51441d82083a9b924" => :mavericks
-    sha256 "5258f37a0149806a78d777c6c311ff9c53eff4d4ae3d14c47825f4c279eed298" => :mountain_lion
+    sha256 "bd0d09c9849ec4ce62bff6bf422cf8c42dfa7d39e684c0d7e7d3d176a749ab76" => :tiger_g3
+    sha256 "032c6cc3f59a97be735e385714da097201d03afb5ecc1ee943d0f5ebf8f06d52" => :tiger_altivec
+    sha256 "23e8ed207c87f4a77e01b49d1972de94795e398ab00e4fe1d2260f1c905306f7" => :tiger_g5
   end
 
   option :universal
