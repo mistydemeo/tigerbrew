a11591ba69f2060c49628bdd4f39bd3a1a9435d3
Misty De Meo <mistydemeo@gmail.com>
2018-05-07 17:03:57 -0700

    readline: add Tiger bottles

--- Library/Formula/readline.rb
+++ Library/Formula/readline.rb
@@ -7,7 +7,8 @@ class Readline < Formula
 
   bottle do
     cellar :any
-    sha256 "f36867f7a2762620894058678f400aa945c9a6930a7a33f7e74d02e12639bbb4" => :tiger_altivec
+    sha256 "5ec9e06a248e6d50c9dfa10ab3a862597fbbb66e79039c2848a6da4995b16af4" => :tiger_g3
+    sha256 "42447f1f1ed5a90e778c74131bb24431438864dccd8fdf1d9f120ee62550da67" => :tiger_altivec
     sha256 "87370403df816b715cb920cce228a31240e7d6eab976bf32b92e2f77050dac06" => :leopard_g3
     sha256 "3d347f4be190c3881c6240f3037e4e5b474b47c36740cf21595ba4122e280205" => :leopard_altivec
   end
