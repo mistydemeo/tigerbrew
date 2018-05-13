d0625075eac01c994e35719cea36f3ff62ed4c90
Misty De Meo <mistydemeo@gmail.com>
2018-05-13 10:21:42 -0700

    tcsh: add Tiger bottles

--- Library/Formula/tcsh.rb
+++ Library/Formula/tcsh.rb
@@ -6,6 +6,9 @@ class Tcsh < Formula
   sha256 "b89de7064ab54dac454a266cfe5d8bf66940cb5ed048d0c30674ea62e7ecef9d"
 
   bottle do
+    sha256 "9ea07dfb5815b5cfda4c022870a31fce1e1e66109d6afd21d45870b6566eef50" => :tiger_g3
+    sha256 "e574d87dd70e6604f675033fcea4bd3f7529242dd868d705a8bae694f87f7a03" => :tiger_altivec
+    sha256 "2ff1a8f5dbd9a71f555009b7ee509dc033953d4558a3fed85f74781a12942c5a" => :tiger_g5
   end
 
   def install
