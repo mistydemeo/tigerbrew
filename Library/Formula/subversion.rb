696a51cbe3d818e7ab94bd1eb601136a3b09ca70
Yong-iL Joh <yong-il.joh@windriver.com>
2019-01-23 23:45:22 +0900

    update to serf-1.3.9
    Signed-off-by: Yong-iL Joh <yong-il.joh@windriver.com>

--- Library/Formula/subversion.rb
+++ Library/Formula/subversion.rb
@@ -29,8 +29,9 @@ class Subversion < Formula
   option "with-gpg-agent", "Build with support for GPG Agent"
 
   resource "serf" do
-    url "https://serf.googlecode.com/svn/src_releases/serf-1.3.8.tar.bz2", :using => :curl
-    sha256 "e0500be065dbbce490449837bb2ab624e46d64fc0b090474d9acaa87c82b2590"
+    url "http://apache.mirror.cdnetworks.com/serf/serf-1.3.9.tar.bz2", :using => :curl
+    mirror "https://archive.apache.org/dist/serf/serf-1.3.9.tar.bz2"
+    sha256 "549c2d21c577a8a9c0450facb5cca809f26591f048e466552240947bdf7a87cc"
   end
 
   # use Tigerbrew's version instead of the old one in X11
