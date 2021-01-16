8b89b84fd40758b505847f2bd414fc28de095171
Misty De Meo <mistydemeo@gmail.com>
2021-01-16 13:39:33 -0800

    Official taps: limit to taps that still exist

--- Library/Homebrew/official_taps.rb
+++ Library/Homebrew/official_taps.rb
@@ -1,18 +1,4 @@
 OFFICIAL_TAPS = %w[
-  apache
-  binary
-  completions
-  devel-only
-  dupes
   emacs
-  fuse
-  games
-  head-only
   nginx
-  php
-  python
-  science
-  tex
-  versions
-  x11
 ]
