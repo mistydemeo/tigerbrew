ffdc9bc53e1e0906d14c6bc66d9ddbb2968ae2d0
Misty De Meo <mistydemeo@gmail.com>
2021-08-24 09:50:20 -0700

    glib: rehost patch
    Fixes #752.

--- Library/Formula/glib.rb
+++ Library/Formula/glib.rb
@@ -60,8 +60,8 @@ class Glib < Formula
   # Fixes g_get_monotonic_time on non-Intel Macs; submitted upstream:
   # https://bugzilla.gnome.org/show_bug.cgi?id=728123
   patch do
-    url "https://bug728123.bugzilla-attachments.gnome.org/attachment.cgi?id=275596"
-    sha1 "5637d98e1c7bbfa8824e60612976a8c13d0c0fb6"
+    url "https://gist.githubusercontent.com/mistydemeo/a34250bb0864a87602f6128784dd87a8/raw/1b0006a1e0cdd2c6740fb0a0223b3104a8ed1c21/g_get_monotonic_time.patch"
+    sha1 "7133b8087952bba71a60cccc2f380aa565c837842b705a90d51ac8beb0fd41cb"
   end
 
   # Reverts GNotification support on macOS.
