563a29556d695a5ad460fe450cec2b563a801399
Misty De Meo <mistydemeo@gmail.com>
2021-10-13 09:28:28 -0700

    jpeg: tiger G4 bottle

--- Library/Formula/jpeg.rb
+++ Library/Formula/jpeg.rb
@@ -6,12 +6,7 @@ class Jpeg < Formula
 
   bottle do
     cellar :any
-    revision 2
-    sha256 "bbc74f8b5980065d7bf95927150c2d56806d30abea459c2b1edcbdeed2d7c458" => :el_capitan
-    sha1 "a0d4d16fcbee7ad6ef49f16bb55650291b877885" => :yosemite
-    sha1 "f668b1e9cb382e194c632c1d5865b7bea096c3ac" => :mavericks
-    sha1 "4dd056f2bf243eef145a613ed1a51e65e4b5d0a4" => :mountain_lion
-    sha1 "396612e00ac31ca730d913ebdfd1b99881304702" => :lion
+    sha256 "b5114438b4cba932adafb931504af522f2111b83e5f76de236870242fb91a9a1" => :tiger_altivec
   end
 
   option :universal
