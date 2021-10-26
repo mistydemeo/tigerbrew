78b7bc5f80ed03efe562bd98daf7687775f03e54
pk2061 <61253042+pk2061@users.noreply.github.com>
2021-10-26 21:38:38 +0200

    Update popt.rb
    I changed the homepage to "https://github.com/rpm-software-management/popt" and the download url to "https://github.com/rpm-software-management/popt/archive/popt-1_16-release.tar.gz" as recommended by @jupo42 in pull request #667.

--- Library/Formula/popt.rb
+++ Library/Formula/popt.rb
@@ -1,16 +1,8 @@
 class Popt < Formula
   desc "Library like getopt(3) with a number of enhancements"
-  homepage "http://rpm5.org"
-  url "https://fossies.org/linux/misc/popt-1.16.tar.gz"
-  sha256 "e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8"
-
-  bottle do
-    revision 1
-    sha256 "60a7f19e8fecafd92a5beb7d6438efac915e8f3afe3d83575fb64bb4a6190aab" => :el_capitan
-    sha1 "20ebf6ad6a0e618c6e14249179ebfaa49ceea1a0" => :yosemite
-    sha1 "ffa33727245492f9583a7e6905bbeef7454b96c8" => :mavericks
-    sha1 "d4736bc9459f25b0d4c267d364798e6614fbbbda" => :mountain_lion
-  end
+  homepage "https://github.com/rpm-software-management/popt"
+  url "https://github.com/rpm-software-management/popt/archive/popt-1_16-release.tar.gz"
+  sha256 "81616cefc007a74cb747d020f058902c7ce6f8c9cec78240f778be96e7c35666"
 
   option :universal
 
