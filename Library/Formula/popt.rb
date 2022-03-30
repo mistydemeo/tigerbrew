707b57d5ed917ecac059c917e2fe005317dca8a6
pk2061 <61253042+pk2061@users.noreply.github.com>
2022-03-31 00:14:52 +0200

    Update popt.rb
    Upgraded popt version in the formula to 1.18.
    Adjusted url and added new mirrors
    Adjusted the sha256 hash
    
    The mirrors and sha256 hash value were taken from the original homebrew formula, see https://github.com/Homebrew/homebrew-core/blob/HEAD/Formula/popt.rb

--- Library/Formula/popt.rb
+++ Library/Formula/popt.rb
@@ -2,8 +2,11 @@ class Popt < Formula
   desc "Library like getopt(3) with a number of enhancements"
   
   homepage "https://github.com/rpm-software-management/popt"
-  url "https://github.com/rpm-software-management/popt/archive/popt-1_16-release.tar.gz"
-  sha256 "81616cefc007a74cb747d020f058902c7ce6f8c9cec78240f778be96e7c35666
+  url "http://ftp.rpm.org/popt/releases/popt-1.x/popt-1.18.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/rpm/popt/releases/popt-1.x/popt-1.18.tar.gz"
+  mirror "https://fossies.org/linux/misc/popt-1.18.tar.gz"
+  
+  sha256 "5159bc03a20b28ce363aa96765f37df99ea4d8850b1ece17d1e6ad5c24fdc5d1"
   option :universal
 
   
