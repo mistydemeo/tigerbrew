66ac249a1d9108ea0b7abb1ae1769606b4764e96
Aaron Blakely <aaron@ephasic.org>
2022-07-05 02:27:12 -0500

    added neofetch formula

---
+++ Library/Formula/neofetch.rb
@@ -0,0 +1,11 @@
+class Neofetch < Formula
+    desc "A CLI system information tool written in BASH"
+    homepage "https://github.com/dylanaraps/neofetch"
+    url "https://github.com/ablakely/neofetch/archive/refs/heads/master.zip"
+    sha256 "6b9d78a0c51524373f6721f051d9cbaba2467818218e6b06a3c63d7ff3ec99e2"
+    option :universal
+
+    def install
+        system "make", "install"
+    end
+end
