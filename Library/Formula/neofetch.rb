1f581867f395c4619790c70ecfb0378add128196
Aaron Blakely <aaron@ephasic.org>
2022-07-05 02:31:46 -0500

    fixed neofetch formula

--- Library/Formula/neofetch.rb
+++ Library/Formula/neofetch.rb
@@ -3,7 +3,7 @@ class Neofetch < Formula
     homepage "https://github.com/dylanaraps/neofetch"
     url "https://github.com/ablakely/neofetch/archive/refs/heads/master.zip"
     sha256 "6b9d78a0c51524373f6721f051d9cbaba2467818218e6b06a3c63d7ff3ec99e2"
-    option :universal
+    version "7.1.0"
 
     def install
         system "make", "install"
