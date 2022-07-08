539e0daf1f2fa443bc938f03ea4d8515a0455c94
Aaron Blakely <aaron@ephasic.org>
2022-07-08 01:23:57 -0500

    switched to 7.1.0 tarball and updated patch

--- Library/Formula/neofetch.rb
+++ Library/Formula/neofetch.rb
@@ -1,28 +1,13 @@
 class Neofetch < Formula
     desc "A CLI system information tool written in BASH"
     homepage "https://github.com/dylanaraps/neofetch"
-    url "https://github.com/dylanaraps/neofetch/archive/refs/heads/master.zip"
-    sha256 "13b188b1fd5bba04bf751c94757c62630c79a553d57680dcae8dcdedd529799a"
+    url "https://github.com/dylanaraps/neofetch/archive/refs/tags/7.1.0.tar.gz"
+    sha256 "58a95e6b714e41efc804eca389a223309169b2def35e57fa934482a6b47c27e7"
     version "7.1.0"
 
     patch do
-        url "https://github.com/ablakely/neofetch/commit/993cfbc62776cb6e17def9ec53751b0876312a1d.patch?full_index=1"
-        sha256 "e06a76dc9adc4a9b04e8159bf036e7a7d8abd00dca64c31053fd093a5c19a4bd"
-    end
-
-    patch do
-        url "https://github.com/ablakely/neofetch/commit/55483a179d2005119ac68c4565014a1bb31ac2c5.patch?full_index=1"
-        sha256 "f0e376abc1af08f9137660fe9fe81767d2920168476d875e9f1c1c71849bbf3c"
-    end
-
-    patch do
-	url "https://github.com/ablakely/neofetch/commit/2a6ca41dcf51a5781714ef63f30ee166f9039934.patch?full_index=1"
-	sha256 "e8eeb40b8a2dd0fce670bbf9a1c95bf7ed7af7a660652ee8cc8fd53e1b617dae"
-    end
-
-    patch do
-        url "https://github.com/ablakely/neofetch/commit/36cc71b08e04ea5a894487a38464efab2e3d4bbb.patch?full_index=1"
-	sha256 "4ac679d68198cb53e5723385ef9e4e53fa94df887cf79e092d43e9e6896561dd"
+        url "https://gist.githubusercontent.com/ablakely/69b1c2c563209e6790a0feddd6b56e70/raw/974c4a6cd35241dcc29b670fb22651021bd8eb76/patch"
+	sha256 "bf0adb97068915400147c18dc9e0f6955ac74cf9e38e1ab7f97dd0b599ea6f50"
     end
 
     def install
