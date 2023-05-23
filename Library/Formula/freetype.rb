2fd55d2de13246f51126b9f74969643124d3347a
Sevan Janiyan <venture37@geeklan.co.uk>
2023-05-23 16:48:36 +0100

    freetype: Upgrade formula to v2.13.0

--- Library/Formula/freetype.rb
+++ Library/Formula/freetype.rb
@@ -1,17 +1,9 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "http://www.freetype.org"
-  url "https://downloads.sf.net/project/freetype/freetype2/2.6/freetype-2.6.tar.bz2"
-  mirror "http://download.savannah.gnu.org/releases/freetype/freetype-2.6.tar.bz2"
-  sha256 "8469fb8124764f85029cc8247c31e132a2c5e51084ddce2a44ea32ee4ae8347e"
-  revision 1
-
-  bottle do
-    cellar :any
-    sha256 "dfee98da99ff2fda0a7056472521ae3cd7947075403268cbffe753bc3bd339aa" => :tiger_altivec
-    sha256 "4cfd87fd6da0daa50a0d75cc325fd9f1e84a68ded25509f4eebc29e45154891e" => :leopard_g3
-    sha256 "24cf7d02f2c219688ee7261ce013305c826619300162617ba209d7f9f735a53a" => :leopard_altivec
-  end
+  url "https://download.savannah.gnu.org/releases/freetype/freetype-2.13.0.tar.xz"
+  mirror "https://downloads.sf.net/project/freetype/freetype2/2.13/freetype-2.13.tar.bz2"
+  sha256 "5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c"
 
   keg_only :provided_pre_mountain_lion
 
@@ -20,17 +12,9 @@ class Freetype < Formula
 
   depends_on "libpng"
 
-  # Don't define a TYPEOF macro in ftconfig.h
-  # https://savannah.nongnu.org/bugs/index.php?45376
-  # http://git.savannah.gnu.org/cgit/freetype/freetype2.git/commit/?id=5931268eecaeda3e05580bdc8885348fecc43fa8
-  patch do
-    url "https://gist.githubusercontent.com/anonymous/b47d77c41a6801879fd2/raw/fc21c3516b465095da7ed13f98bea491a7d18bbd/patch"
-    sha256 "5b21575d0384c9e502b51b0ba4be0ff453a34bcf9deba52b6baa38c3ffcde063"
-  end
-
   def install
     if build.with? "subpixel"
-      inreplace "include/config/ftoption.h",
+      inreplace "include/freetype/config/ftoption.h",
           "/* #define FT_CONFIG_OPTION_SUBPIXEL_RENDERING */",
           "#define FT_CONFIG_OPTION_SUBPIXEL_RENDERING"
     end
