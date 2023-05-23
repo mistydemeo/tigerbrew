af00daa303bf8d858e34f3f11e42352cc063beb5
Sevan Janiyan <venture37@geeklan.co.uk>
2023-05-23 15:34:42 +0100

    oniguruma: Upgrade formula to v6.9.8

--- Library/Formula/oniguruma.rb
+++ Library/Formula/oniguruma.rb
@@ -1,17 +1,8 @@
 class Oniguruma < Formula
   desc "Regular expressions library"
   homepage "https://github.com/kkos/oniguruma/"
-  url "https://github.com/kkos/oniguruma/releases/download/v5.9.6/onig-5.9.6.tar.gz"
-  sha256 "d5642010336a6f68b7f2e34b1f1cb14be333e4d95c2ac02b38c162caf44e47a7"
-
-  bottle do
-    cellar :any
-    revision 1
-    sha256 "653e3734097028be0863cdab51a8d3c92c1e3206cfec50072c18ab9e4e36bfc3" => :el_capitan
-    sha256 "6a645e4c7bcf88d3d50aee632de816d884af7edf094fe793b451256e55bb8531" => :yosemite
-    sha256 "ed3f1ef356a9dcae1ffff0b9bd853314b07289dfacdbf6b403515bcd35233227" => :mavericks
-    sha256 "5a2ac152702eea6000bfa493607393f610fbec9684f1852cceb87834f20c9321" => :mountain_lion
-  end
+  url "https://github.com/kkos/oniguruma/releases/download/v6.9.8/onig-6.9.8.tar.gz"
+  sha256 "28cd62c1464623c7910565fb1ccaaa0104b2fe8b12bcd646e81f73b47535213e"
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
