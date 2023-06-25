061a4e2b874caa054da628609128b89bbfc25f7f
Sevan Janiyan <venture37@geeklan.co.uk>
2023-06-25 17:19:16 +0100

    cyrus-sasl 2.1.28
    Build strugles with krb5_gss_register_acceptor_identity definition,
    skip Kerberos support.

--- Library/Formula/cyrus-sasl.rb
+++ Library/Formula/cyrus-sasl.rb
@@ -1,23 +1,19 @@
 require 'formula'
 
 class CyrusSasl < Formula
-  # This formula uses 2.1.22 because, while later versions build on Tiger,
-  # they don't seem to work properly when sending mail
   homepage 'http://cyrusimap.org'
-  url 'ftp://ftp.cyrusimap.org/cyrus-sasl/OLD-VERSIONS/cyrus-sasl-2.1.22.tar.gz'
-  sha1 'd23454ab12054714ab97d229c86cb934ce63fbb1'
+  url "https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-2.1.28/cyrus-sasl-2.1.28.tar.gz"
+  sha256 "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
 
   keg_only :provided_by_osx
+
+  depends_on "openssl"
 
   def install
     system "./configure",
            "--disable-macos-framework",
-           "--prefix=#{prefix}"
-           # The below are for 2.1.26, which built but wouldn't work
-           # with sending and gmail.  The flags were necessary to get
-           # a succesful build, IIRC.
-           #"--disable-scram",
-           #"--disable-gssapi",
+           "--prefix=#{prefix}",
+           "--disable-gssapi"
     system "make"
     system "make check"
     system "make install"
