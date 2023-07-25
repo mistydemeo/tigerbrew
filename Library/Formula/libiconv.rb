c526554d0512721a47fe034b4e5a700bbcd738d7
Sevan Janiyan <venture37@geeklan.co.uk>
2023-07-25 22:11:03 +0100

    libiconv: Upgrade formula to v1.17

--- Library/Formula/libiconv.rb
+++ Library/Formula/libiconv.rb
@@ -1,16 +1,9 @@
 class Libiconv < Formula
   desc "Conversion library"
   homepage "https://www.gnu.org/software/libiconv/"
-  url "https://ftpmirror.gnu.org/libiconv/libiconv-1.15.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libiconv/libiconv-1.15.tar.gz"
-  sha256 "ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178"
-
-  bottle do
-    cellar :any
-    sha256 "b3594af5de946fdbaa5d45a1ab2107b82f81e689ac15c13ad7ad309aef32780e" => :sierra
-    sha256 "731b3b0d234747a3fdd8b7152d46bba4d790acb55f411ac9970e448b43fe2457" => :el_capitan
-    sha256 "c3fd281560ecc86d2453d4916a67267207dff4c19baeb42ce2db8cfbfb52eadb" => :yosemite
-  end
+  url "https://ftpmirror.gnu.org/libiconv/libiconv-1.17.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/libiconv/libiconv-1.17.tar.gz"
+  sha256 "8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313"
 
   keg_only :provided_by_osx
 
@@ -23,7 +16,6 @@ class Libiconv < Formula
 
   def install
     ENV.deparallelize
-
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
