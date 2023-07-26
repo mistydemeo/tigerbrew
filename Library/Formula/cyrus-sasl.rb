e2ea00373a386b6cbd49c504512bd4bed177d6f2
Misty De Meo <mistydemeo@gmail.com>
2023-07-26 13:26:14 -0700

    cyrus-sasl: add bottle

--- Library/Formula/cyrus-sasl.rb
+++ Library/Formula/cyrus-sasl.rb
@@ -5,6 +5,10 @@ class CyrusSasl < Formula
   url "https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-2.1.28/cyrus-sasl-2.1.28.tar.gz"
   sha256 "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
 
+  bottle do
+    sha256 "e2387b76b95e32bf97a0f1433eb882225b2d027eb4c3717a37afc463225a885b" => :tiger_altivec
+  end
+
   keg_only :provided_by_osx
 
   depends_on "openssl"
