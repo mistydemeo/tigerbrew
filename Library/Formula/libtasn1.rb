42b44c115b2fa6a8f80c677540ae54b03c15f57f
Sevan Janiyan <venture37@geeklan.co.uk>
2023-07-28 21:53:34 +0100

    libtasn1 4.19.0

--- Library/Formula/libtasn1.rb
+++ Library/Formula/libtasn1.rb
@@ -1,16 +1,9 @@
 class Libtasn1 < Formula
   desc "ASN.1 structure parser library"
   homepage "https://www.gnu.org/software/libtasn1/"
-  url "http://ftpmirror.gnu.org/libtasn1/libtasn1-4.7.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.7.tar.gz"
-  sha256 "a40780dc93fc6d819170240e8ece25352058a85fd1d2347ce0f143667d8f11c9"
-
-  bottle do
-    cellar :any
-    sha256 "50cc8266e6239b597ee419b5437f3f8423c77999f4dcd6c12165495c324a65b0" => :tiger_altivec
-    sha256 "c3db02178bb7db0112d8c4798332f2472057c9a56826dea60f8021b2cacece5f" => :leopard_g3
-    sha256 "093f4d5a8774d7e0572233b814e32c2ebe2647ccbc9661adaf7cda18c0383b10" => :leopard_altivec
-  end
+  url "https://ftpmirror.gnu.org/libtasn1/libtasn1-4.19.0.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.19.0.tar.gz"
+  sha256 "1613f0ac1cf484d6ec0ce3b8c06d56263cc7242f1c23b30d82d23de345a63f7a"
 
   option :universal
 
@@ -18,6 +11,7 @@ class Libtasn1 < Formula
     ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking",
                           "--disable-silent-rules"
+    system "make", "check"
     system "make", "install"
   end
 
