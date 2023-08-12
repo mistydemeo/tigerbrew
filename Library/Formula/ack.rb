34725155c005f5b473c101638dc0af4e1941a05a
Sevan Janiyan <venture37@geeklan.co.uk>
2023-08-12 16:21:35 +0100

    ack 3.7.0

--- Library/Formula/ack.rb
+++ Library/Formula/ack.rb
@@ -1,26 +1,23 @@
 class Ack < Formula
   desc "Search tool like grep, but optimized for programmers"
-  homepage "http://beyondgrep.com/"
-  url "http://beyondgrep.com/ack-2.14-single-file"
-  sha256 "1d203cfbc52ce8f49e3992be1cd3e4d7d5dfb7daa3739e8628aa9858ccc5b9df"
-  version "2.14"
+  homepage "https://beyondgrep.com"
+  url "https://beyondgrep.com/ack-v3.7.0"
+  sha256 "dd5a7c2df81ee15d97b6bf6b3ff84ad2529c98e1571817861c7d4fd8d48af908"
+  version "3.7.0"
 
-  head "https://github.com/petdance/ack2.git", :branch => "dev"
-
-  devel do
-    url "https://cpan.metacpan.org/authors/id/P/PE/PETDANCE/ack-2.15_01.tar.gz"
-    sha256 "dfd1df3def5d3b16af8a7c585fc8954362d4f2b097891919490c89fdb484fd83"
-    version "2.15-01"
-  end
+  head "https://github.com/beyondgrep/ack3.git", :branch => "dev"
 
   resource "File::Next" do
-    url "https://cpan.metacpan.org/authors/id/P/PE/PETDANCE/File-Next-1.12.tar.gz"
-    sha256 "cc3afd8eaf6294aba93b8152a269cc36a9df707c6dc2c149aaa04dabd869e60a"
+    url "https://cpan.metacpan.org/authors/id/P/PE/PETDANCE/File-Next-1.18.tar.gz"
+    sha256 "f900cb39505eb6e168a9ca51a10b73f1bbde1914b923a09ecd72d9c02e6ec2ef"
   end
+
+  depends_on "perl" # Need Perl >= 5.10.1
 
   def install
     if build.stable?
-      bin.install "ack-#{version}-single-file" => "ack"
+      inreplace "ack-v#{version}", "/usr/bin/env perl", "#{Formula["perl"].bin}/perl"
+      bin.install "ack-v#{version}" => "ack"
       system "pod2man", "#{bin}/ack", "ack.1"
       man1.install "ack.1"
     else
