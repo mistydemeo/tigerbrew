dea978e201b37ec9f052266a8c67ac7ba221dbc4
Misty De Meo <mistydemeo@gmail.com>
2023-08-11 22:31:03 -0700

    libatomic_ops: add bottle

--- Library/Formula/libatomic_ops.rb
+++ Library/Formula/libatomic_ops.rb
@@ -6,11 +6,7 @@ class LibatomicOps < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 2
-    sha256 "cbb473e519b1a3ca0652a994b27d0e1e79be1a620e0a633f81a613127fb6ed44" => :el_capitan
-    sha256 "9a149cc8ba1978bd27eeac295ba0b68026d32b4f732f61937534435cbf3e5558" => :yosemite
-    sha256 "74f72421aaf164af27890dc4d53d87fd1e06ca175c611d474b782afd967cd399" => :mavericks
-    sha256 "b9d20bfe742eb48b6bc491459ee20f69acde47e757b03817544e79c5be80ec61" => :mountain_lion
+    sha256 "ab9fd979995caeea176d81867fe3fb131aeecaa2db4713a4bbd193089100edc3" => :tiger_altivec
   end
 
   def install
