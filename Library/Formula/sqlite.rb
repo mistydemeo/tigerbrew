6299c39990f1b4846efff5b9b116c343ab3d5f0b
Sevan Janiyan <venture37@geeklan.co.uk>
2023-08-18 23:07:40 +0100

    sqlite 3.42.0
    Tested on
    Leopard i386 & ppc, GCC 4.2
    Tiger ppc, GCC 4.0.1

--- Library/Formula/sqlite.rb
+++ Library/Formula/sqlite.rb
@@ -1,15 +1,9 @@
 class Sqlite < Formula
   desc "Command-line interface for SQLite"
   homepage "https://sqlite.org/"
-  url "https://sqlite.org/2016/sqlite-autoconf-3150000.tar.gz"
-  version "3.15.0"
-  sha256 "77162da9b4a0336d7e77d5252b690662850f62b47c12d9125f74ab9de78ded27"
-  revision 1
-
-  bottle do
-    cellar :any
-    sha256 "b5890aabd2970eb1b55ab57a40cd609f5f124e80128b2efcb22b8a262c110f29" => :tiger_altivec
-  end
+  url "https://www.sqlite.org/2023/sqlite-autoconf-3420000.tar.gz"
+  version "3.42.0"
+  sha256 "7abcfd161c6e2742ca5c6c0895d1f853c940f203304a0b49da4e1eca5d088ca6"
 
   keg_only :provided_by_osx, "OS X provides an older sqlite3."
 
@@ -36,14 +30,10 @@ class Sqlite < Formula
   end
 
   resource "docs" do
-    url "https://sqlite.org/2016/sqlite-doc-3150000.zip"
-    version "3.15.0"
-    sha256 "cea0208b2b8dddf51b846d6e9d23210fc497da2c6f3fbed96f00eadceb1c32f7"
+    url "https://www.sqlite.org/2023/sqlite-doc-3420000.zip"
+    version "3.42.0"
+    sha256 "d62fd5f0fcfd15b2c26171e89f762268e2863df3c09696e76cf1482e34db8262"
   end
-
-  # sqlite won't compile on Tiger due to missing function;
-  # patch submitted upstream: http://thread.gmane.org/gmane.comp.db.sqlite.general/83257
-  def patches; DATA; end if MacOS.version < :leopard
 
   def install
     # sqlite segfaults on Tiger/PPC with our gcc-4.2
@@ -125,54 +115,3 @@ class Sqlite < Formula
     assert_equal %w[Sue Tim Bob], names
   end
 end
-
-__END__
---- a/sqlite3.c  2013-08-27 18:37:13.000000000 -0700
-+++ b/sqlite3.c 2013-08-27 21:25:45.000000000 -0700
-@@ -15685,6 +15685,7 @@
- #include <sys/sysctl.h>
- #include <malloc/malloc.h>
- #include <libkern/OSAtomic.h>
-+
- static malloc_zone_t* _sqliteZone_;
- #define SQLITE_MALLOC(x) malloc_zone_malloc(_sqliteZone_, (x))
- #define SQLITE_FREE(x) malloc_zone_free(_sqliteZone_, (x));
-@@ -15692,6 +15693,29 @@
- #define SQLITE_MALLOCSIZE(x) \
-         (_sqliteZone_ ? _sqliteZone_->size(_sqliteZone_,x) : malloc_size(x))
- 
-+/*
-+** If compiling for Mac OS X 10.4, the OSAtomicCompareAndSwapPtrBarrier
-+** function will not be available, but individual 32-bit and 64-bit
-+** versions will.
-+*/
-+
-+#ifdef __MAC_OS_X_MIN_REQUIRED
-+# include <AvailabilityMacros.h>
-+#elif defined(__IPHONE_OS_MIN_REQUIRED)
-+# include <Availability.h>
-+#endif
-+
-+typedef int fc_atomic_int_t;
-+#if (MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_4 || __IPHONE_VERSION_MIN_REQUIRED >= 20100)
-+# define fc_atomic_ptr_cmpexch(O,N,P) OSAtomicCompareAndSwapPtrBarrier ((void *) (O), (void *) (N), (void **) (P))
-+#else
-+# if __ppc64__ || __x86_64__
-+#  define fc_atomic_ptr_cmpexch(O,N,P) OSAtomicCompareAndSwap64Barrier ((int64_t) (O), (int64_t) (N), (int64_t*) (P))
-+# else
-+#  define fc_atomic_ptr_cmpexch(O,N,P) OSAtomicCompareAndSwap32Barrier ((int32_t) (O), (int32_t) (N), (int32_t*) (P))
-+# endif
-+#endif
-+
- #else /* if not __APPLE__ */
- 
- /*
-@@ -15852,7 +15876,7 @@
-     malloc_zone_t* newzone = malloc_create_zone(4096, 0);
-     malloc_set_zone_name(newzone, "Sqlite_Heap");
-     do{
--      success = OSAtomicCompareAndSwapPtrBarrier(NULL, newzone, 
-+      success = fc_atomic_ptr_cmpexch(NULL, newzone, 
-                                  (void * volatile *)&_sqliteZone_);
-     }while(!_sqliteZone_);
-     if( !success ){
