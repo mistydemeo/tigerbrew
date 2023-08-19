df0dd26c5121858f026e7747651c91c7a6134d17
Sevan Janiyan <venture37@geeklan.co.uk>
2023-08-19 02:40:48 +0100

    ntp 4.2.8p17
    Adapted from
    https://raw.githubusercontent.com/Homebrew/homebrew-core/a5a3274dc89f48d7853241955660e00b479e473e/Formula/n/ntp.rb

---
+++ Library/Formula/ntp.rb
@@ -0,0 +1,30 @@
+class Ntp < Formula
+  desc "Network Time Protocol (NTP) Distribution"
+  homepage "https://www.ntp.org"
+  url "https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.8p17.tar.gz"
+  version "4.2.8p17"
+  sha256 "103dd272e6a66c5b8df07dce5e9a02555fcd6f1397bdfb782237328e89d3a866"
+  license all_of: ["BSD-2-Clause", "NTP"]
+
+  depends_on "openssl"
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-openssl-libdir=#{Formula["openssl"].lib}
+      --with-openssl-incdir=#{Formula["openssl"].include}
+      --with-net-snmp-config=no
+    ]
+
+    system "./configure", *args
+    ldflags = "-lresolv -undefined dynamic_lookup"
+    system "make", "install", "LDADD_LIBNTP=#{ldflags}"
+  end
+
+  test do
+    assert_match "step time server ", shell_output("#{sbin}/ntpdate -bq pool.ntp.org")
+  end
+end
