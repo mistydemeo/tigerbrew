4780193baf3210fb90186a17954d7a7dd57ca078
Misty De Meo <mistydemeo@gmail.com>
2023-08-18 17:13:50 -0700

    lzo: add bottle

--- Library/Formula/lzo.rb
+++ Library/Formula/lzo.rb
@@ -4,6 +4,10 @@ class Lzo < Formula
   url "http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz"
   sha256 "c0f892943208266f9b6543b3ae308fab6284c5c90e627931446fb49b4221a072"
 
+  bottle do
+    sha256 "425fd8959623501a81d361dd99029e34ea53025f2e4cab75c302c6840c9d4b58" => :tiger_altivec
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
