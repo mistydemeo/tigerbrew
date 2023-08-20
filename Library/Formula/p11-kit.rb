1eb1b46b87f815e13cd03e0055fc9479602df162
Misty De Meo <mistydemeo@gmail.com>
2023-08-20 16:44:05 -0700

    p11-kit: add bottle

--- Library/Formula/p11-kit.rb
+++ Library/Formula/p11-kit.rb
@@ -4,6 +4,10 @@ class P11Kit < Formula
   url "https://github.com/p11-glue/p11-kit/releases/download/0.24.1/p11-kit-0.24.1.tar.xz"
   sha256 "d8be783efd5cd4ae534cee4132338e3f40f182c3205d23b200094ec85faaaef8"
 
+  bottle do
+    sha256 "25803e9ddf676302940bfb1612f53cdd6cbce52c10caef302508131e6468a8d3" => :tiger_altivec
+  end
+
   option :universal
 
   depends_on "pkg-config" => :build
