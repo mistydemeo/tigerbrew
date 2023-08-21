e087bafb7e587a5ad11ed320e12b7ad17dba442d
Misty De Meo <mistydemeo@gmail.com>
2023-08-20 21:29:39 -0700

    ddrescue: add bottle

--- Library/Formula/ddrescue.rb
+++ Library/Formula/ddrescue.rb
@@ -5,6 +5,10 @@ class Ddrescue < Formula
   mirror "https://ftp.gnu.org/gnu/ddrescue/ddrescue-1.27.tar.lz"
   sha256 "38c80c98c5a44f15e53663e4510097fd68d6ec20758efdf3a925037c183232eb"
 
+  bottle do
+    sha256 "817916afa65a7f92c8a489634d947e9968a42ff63210a7049023e04265531a0a" => :tiger_altivec
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "CXX=#{ENV.cxx}"
