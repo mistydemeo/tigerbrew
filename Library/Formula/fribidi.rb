bec23857ce7c5ab49ab1f4207540db9f96d5dbdf
Misty De Meo <mistydemeo@gmail.com>
2023-08-20 22:30:03 -0700

    fribidi: add bottle

--- Library/Formula/fribidi.rb
+++ Library/Formula/fribidi.rb
@@ -4,6 +4,10 @@ class Fribidi < Formula
   url "https://github.com/fribidi/fribidi/releases/download/v1.0.13/fribidi-1.0.13.tar.xz"
   sha256 "7fa16c80c81bd622f7b198d31356da139cc318a63fc7761217af4130903f54a2"
 
+  bottle do
+    sha256 "4dd469c270906cb2c620d735ffbb192625ef60a107387c14dd95a27fb831d17c" => :tiger_altivec
+  end
+
   option :universal
 
   def install
