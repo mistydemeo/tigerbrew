9e7c232a85ffbad47b366ef5add47921cff313fd
Misty De Meo <mistydemeo@gmail.com>
2023-08-20 20:55:29 -0700

    jpeg: add bottle

--- Library/Formula/jpeg.rb
+++ Library/Formula/jpeg.rb
@@ -4,6 +4,10 @@ class Jpeg < Formula
   url "http://www.ijg.org/files/jpegsrc.v9e.tar.gz"
   sha256 "4077d6a6a75aeb01884f708919d25934c93305e49f7e3f36db9129320e6f4f3d"
 
+  bottle do
+    sha256 "fa53681fb9e9c318b341868952b478627e09b422dd3a3ec471869d8af434c6f1" => :tiger_altivec
+  end
+
   option :universal
 
   def install
