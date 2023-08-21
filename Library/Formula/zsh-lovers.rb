481e43dd1b27c0de2cf8b67c1fafb23e38d547e9
Sevan Janiyan <venture37@geeklan.co.uk>
2023-08-21 02:42:28 +0100

    zsh-lovers 0.9.1 (#881)

--- Library/Formula/zsh-lovers.rb
+++ Library/Formula/zsh-lovers.rb
@@ -2,8 +2,8 @@ class ZshLovers < Formula
   desc "Tips, tricks, and examples for zsh"
   homepage "http://grml.org/zsh/#zshlovers"
   url "http://grml.org/zsh/zsh-lovers.1"
-  version "0.9.0"
-  sha256 "b40802dd83f1f2eded9823776ae90af8e0b91488e97f98b3a0629f8d11953e81"
+  version "0.9.1"
+  sha256 "6583aabf4024c951f19a381ce97ce2c7af948b24fc332504c3f719ec9187c72f"
 
   def install
     man1.install "zsh-lovers.1"
