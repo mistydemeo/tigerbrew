866644436b5fbddad09eb19b163c84a698c26aa4
Larry Davis <lazdnet@gmail.com>
2023-08-23 09:02:53 -0700

    zlib: fix url (#942)

--- Library/Formula/zlib.rb
+++ Library/Formula/zlib.rb
@@ -1,7 +1,7 @@
 class Zlib < Formula
   desc "General-purpose lossless data-compression library"
   homepage "http://www.zlib.net/"
-  url "http://zlib.net/zlib-1.2.13.tar.gz"
+  url "http://zlib.net/fossils/zlib-1.2.13.tar.gz"
   sha256 "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
 
   bottle do
