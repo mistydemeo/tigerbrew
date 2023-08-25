4d88b490d239e670665b279d0c06b5814de780b1
Sevan Janiyan <venture37@geeklan.co.uk>
2023-08-25 17:24:38 +0100

    zlib 1.3

--- Library/Formula/zlib.rb
+++ Library/Formula/zlib.rb
@@ -1,8 +1,8 @@
 class Zlib < Formula
   desc "General-purpose lossless data-compression library"
   homepage "http://www.zlib.net/"
-  url "http://zlib.net/fossils/zlib-1.2.13.tar.gz"
-  sha256 "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
+  url "https://zlib.net/fossils/zlib-1.3.tar.gz"
+  sha256 "ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e"
 
   bottle do
     cellar :any
