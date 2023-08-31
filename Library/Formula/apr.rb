d90790293445165b656b89eee3a640f0848e7833
Misty De Meo <mistydemeo@gmail.com>
2023-08-30 20:20:52 -0700

    apr: add bottle

--- Library/Formula/apr.rb
+++ Library/Formula/apr.rb
@@ -4,6 +4,10 @@ class Apr < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=apr/apr-1.7.4.tar.bz2"
   sha256 "fc648de983f3a2a6c9e78dea1f180639bd2fad6c06d556d4367a701fe5c35577"
 
+  bottle do
+    sha256 "ed0e835943921e6bcf6c885069ffc4e6c74753217d7f0936fa4e2d7389af09f9" => :tiger_altivec
+  end
+
   keg_only :provided_by_osx, "Apple's CLT package contains apr."
 
   option :universal
