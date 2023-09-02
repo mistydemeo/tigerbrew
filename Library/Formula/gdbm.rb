076cc990f6d6aafa4bcc205b006caf16f59fea47
Misty De Meo <mistydemeo@gmail.com>
2023-09-02 12:51:10 -0700

    gdbm: add bottle

--- Library/Formula/gdbm.rb
+++ Library/Formula/gdbm.rb
@@ -5,6 +5,11 @@ class Gdbm < Formula
   mirror "http://ftpmirror.gnu.org/gdbm/gdbm-1.23.tar.gz"
   sha256 "74b1081d21fff13ae4bd7c16e5d6e504a4c26f7cde1dca0d963a484174bbcacd"
 
+  bottle do
+    cellar :any
+    sha256 "adfa78e136cc5c3cddf6b4d2b953a072eda010d22677d869ec7996d70cbb504f" => :tiger_altivec
+  end
+
   option :universal
   option "with-libgdbm-compat", "Build libgdbm_compat, a compatibility layer which provides UNIX-like dbm and ndbm interfaces."
 
