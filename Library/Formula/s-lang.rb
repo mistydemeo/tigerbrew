d35dcd8a5fedc5872c313cc42a5a56bb73983904
Misty De Meo <mistydemeo@gmail.com>
2023-09-02 13:31:45 -0700

    slang: add bottle

--- Library/Formula/s-lang.rb
+++ Library/Formula/s-lang.rb
@@ -4,6 +4,10 @@ class SLang < Formula
   url "http://www.jedsoft.org/releases/slang/slang-2.3.3.tar.bz2"
   sha256 "f9145054ae131973c61208ea82486d5dd10e3c5cdad23b7c4a0617743c8f5a18"
 
+  bottle do
+    sha256 "2cc2cc6f075322895ca0c101e2d37d3c401984c2026ef1ab066fb2a6a379c9e2" => :tiger_altivec
+  end
+
   depends_on "libpng"
   depends_on "pcre" => :optional
   depends_on "oniguruma" => :optional
