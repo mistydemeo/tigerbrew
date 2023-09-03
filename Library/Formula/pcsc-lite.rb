80f2c0659651267938f9773ccde7d929386aca37
Sevan Janiyan <venture37@geeklan.co.uk>
2023-09-03 14:47:20 +0100

    pcsc-lite update urls

--- Library/Formula/pcsc-lite.rb
+++ Library/Formula/pcsc-lite.rb
@@ -1,7 +1,7 @@
 class PcscLite < Formula
   desc "Middleware to access a smart card using SCard API"
-  homepage "https://pcsclite.alioth.debian.org"
-  url "https://alioth.debian.org/frs/download.php/file/4138/pcsc-lite-1.8.14.tar.bz2"
+  homepage "https://pcsclite.apdu.fr"
+  url "https://pcsclite.apdu.fr/files/pcsc-lite-1.8.14.tar.bz2"
   sha256 "b91f97806042315a41f005e69529cb968621f73f2ddfbd1380111a175b02334e"
 
   bottle do
