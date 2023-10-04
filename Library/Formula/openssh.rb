9aa8dd76a3c2d05221af1687881526f433fa4c0a
Sevan Janiyan <venture37@geeklan.co.uk>
2023-10-04 13:20:15 +0100

    openssh 9.5p1

--- Library/Formula/openssh.rb
+++ Library/Formula/openssh.rb
@@ -1,14 +1,13 @@
 class Openssh < Formula
   desc "OpenBSD freely-licensed SSH connectivity tools"
   homepage "https://www.openssh.com/"
-  url "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.4p1.tar.gz"
-  mirror "https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.4p1.tar.gz"
-  version "9.4p1"
-  sha256 "3608fd9088db2163ceb3e600c85ab79d0de3d221e59192ea1923e23263866a85"
+  url "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.5p1.tar.gz"
+  mirror "https://cloudflare.cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.5p1.tar.gz"
+  version "9.5p1"
+  sha256 "f026e7b79ba7fb540f75182af96dc8a8f1db395f922bbc9f6ca603672686086b"
   license "SSH-OpenSSH"
 
   bottle do
-    sha256 "ad3accf8ec0c1325f9fbcee0e11c99e7e7b7556931a80d6d4c2cefe49902d76f" => :tiger_altivec
   end
 
   # Please don't resubmit the keychain patch option. It will never be accepted.
@@ -37,16 +36,6 @@ class Openssh < Formula
     sha256 "a273f86360ea5da3910cfa4c118be931d10904267605cdd4b2055ced3a829774"
   end
 
-  # Fix zlib version check for 1.3 and future version.
-  # This can be removed in next version
-  patch do
-    url "https://github.com/openssh/openssh-portable/commit/cb4ed12ffc332d1f72d054ed92655b5f1c38f621.diff"
-    sha256 "60d057bc1752ac6802f9e9cd160c9838678b9e2e615b441ef6cc8ac8cb28f47f"
-  end
-  # Patch configure to avoid autoreconf
-  # This can be removed in next version
-  patch :p0, :DATA
-
   def install
     args = %W[
       --prefix=#{prefix}
@@ -59,13 +48,11 @@ class Openssh < Formula
       --with-zlib=#{Formula["zlib"].opt_prefix}
     ]
 
-    if OS.mac?
-      ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
+    ENV.append "CPPFLAGS", "-D__APPLE_SANDBOX_NAMED_EXTERNAL__"
 
-      # Ensure sandbox profile prefix is correct.
-      # We introduce this issue with patching, it's not an upstream bug.
-      inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
-    end
+    # Ensure sandbox profile prefix is correct.
+    # We introduce this issue with patching, it's not an upstream bug.
+    inreplace "sandbox-darwin.c", "@PREFIX@/share/openssh", etc/"ssh"
 
     system "./configure", *args
     system "make"
@@ -90,15 +77,3 @@ class Openssh < Formula
     assert_match "sshd", shell_output("lsof -i :#{port}")
   end
 end
-__END__
---- configure.orig	2023-09-14 00:32:20.000000000 +0100
-+++ configure	2023-09-14 00:33:10.000000000 +0100
-@@ -10889,7 +10889,7 @@
- 
- 	int a=0, b=0, c=0, d=0, n, v;
- 	n = sscanf(ZLIB_VERSION, "%d.%d.%d.%d", &a, &b, &c, &d);
--	if (n != 3 && n != 4)
-+	if (n < 1)
- 		exit(1);
- 	v = a*1000000 + b*10000 + c*100 + d;
- 	fprintf(stderr, "found zlib version %s (%d)\n", ZLIB_VERSION, v);
