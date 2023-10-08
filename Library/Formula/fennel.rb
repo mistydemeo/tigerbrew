6dd8e5fd3e47216cfa150bf0e6b278ba877a9c04
Misty De Meo <mistydemeo@gmail.com>
2023-10-08 16:17:52 -0700

    fennel: add bottle

--- Library/Formula/fennel.rb
+++ Library/Formula/fennel.rb
@@ -6,6 +6,8 @@ class Fennel < Formula
   license "MIT"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "1df9168b5ed9f68a98c9c5cd67798aefeb33a01fda717e86a4a4bb4188b0f650" => :tiger_g3
   end
 
   depends_on "lua"
