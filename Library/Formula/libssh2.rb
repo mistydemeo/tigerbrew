3cc45128b65b3c49ddfd6ac82d02c6ac43ad85e1
Sevan Janiyan <venture37@geeklan.co.uk>
2023-10-26 17:55:06 +0100

    libssh2: use openssl3

--- Library/Formula/libssh2.rb
+++ Library/Formula/libssh2.rb
@@ -3,9 +3,9 @@ class Libssh2 < Formula
   homepage "http://www.libssh2.org/"
   url "https://libssh2.org/download/libssh2-1.11.0.tar.xz"
   sha256 "a488a22625296342ddae862de1d59633e6d446eff8417398e06674a49be3d7c2"
+  revision 1
 
   bottle do
-    sha256 "eb70ab12fe7e6a56beaaa7ff9ef43eb43d7c8817286a8086f9786bf76904ad24" => :tiger_altivec
   end
 
   option "with-libressl", "build with LibreSSL instead of OpenSSL"
@@ -18,7 +18,7 @@ class Libssh2 < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on "openssl" => :recommended
+  depends_on "openssl3" => :recommended
   depends_on "libressl" => :optional
   depends_on "zlib"
 
@@ -36,7 +36,7 @@ class Libssh2 < Formula
     if build.with? "libressl"
       args << "--with-libssl-prefix=#{Formula["libressl"].opt_prefix}"
     else
-      args << "--with-libssl-prefix=#{Formula["openssl"].opt_prefix}"
+      args << "--with-libssl-prefix=#{Formula["openssl3"].opt_prefix}"
     end
 
     system "./buildconf" if build.head?
