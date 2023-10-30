ba00b93d2fbbc7981dac09a84dc37fe1864370ef
Misty De Meo <mistydemeo@gmail.com>
2023-10-29 22:36:16 -0700

    norm: add mirror

--- Library/Formula/norm.rb
+++ Library/Formula/norm.rb
@@ -2,6 +2,7 @@ class Norm < Formula
   desc "NACK-Oriented Reliable Multicast"
   homepage "https://www.nrl.navy.mil/itd/ncs/products/norm"
   url "https://downloads.pf.itd.nrl.navy.mil/norm/archive/src-norm-1.5r6.tgz"
+  mirror "https://web.archive.org/web/20161223160402/https://downloads.pf.itd.nrl.navy.mil/norm/archive/src-norm-1.5r6.tgz"
   version "1.5r6"
   sha256 "20ea2e8dd5d5e1ff1ff91dc7dab6db53a77d7b7183d8cf2425c215fd294f22a7"
 
