f457dbba934422928a0361f1db2c12cd6235405e
Misty De Meo <mistydemeo@gmail.com>
2023-10-29 22:34:59 -0700

    libpgm: add mirror

--- Library/Formula/libpgm.rb
+++ Library/Formula/libpgm.rb
@@ -2,6 +2,7 @@ class Libpgm < Formula
   desc "Implements the PGM reliable multicast protocol"
   homepage "https://code.google.com/p/openpgm/"
   url "https://openpgm.googlecode.com/files/libpgm-5.2.122%7Edfsg.tar.gz"
+  mirror "https://web.archive.org/web/20160803065518/https://openpgm.googlecode.com/files/libpgm-5.2.122~dfsg.tar.gz"
   sha256 "e296f714d7057e3cdb87f4e29b1aecb3b201b9fcb60aa19ed4eec29524f08bd8"
   version "5.2.122"
 
