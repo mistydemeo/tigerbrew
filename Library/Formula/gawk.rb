7ded68045f382e4b6a0efee723126b58b10c2bbf
Misty De Meo <mistydemeo@gmail.com>
2023-11-13 18:23:15 -0800

    gawk: add bottle

--- Library/Formula/gawk.rb
+++ Library/Formula/gawk.rb
@@ -6,6 +6,7 @@ class Gawk < Formula
   sha256 "ca9c16d3d11d0ff8c69d79dc0b47267e1329a69b39b799895604ed447d3ca90b"
 
   bottle do
+    sha256 "ce5c5661489afa9de16ea5df4e571e415bc7c4c3b63e035da43631c26cb9c0cb" => :tiger_altivec
   end
 
   def install
