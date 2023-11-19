783ff01342bf0d75f1ea8b4fd330937367d56379
Sevan Janiyan <venture37@geeklan.co.uk>
2023-11-19 14:18:18 +0000

    autoconf 2.71

--- Library/Formula/autoconf.rb
+++ Library/Formula/autoconf.rb
@@ -1,15 +1,12 @@
 class Autoconf < Formula
   desc "Automatic configure script builder"
   homepage "https://www.gnu.org/software/autoconf"
-  url "http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
-  sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
+  url "https://ftpmirror.gnu.org/autoconf/autoconf-2.71.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.71.tar.xz"
+  sha256 "f14c83cfebcc9427f2c3cea7258bd90df972d92eb26752da4ddad81c87a0faa4"
 
   bottle do
     cellar :any_skip_relocation
-    revision 5
-    sha256 "2c9ac7548aa73b102ae653c71deed601b77bf89eff34f04c71cd0fc66099ecfb" => :tiger_g4e
-    sha256 "e370351723235b2d5df402818b9073710afd66f9d9b1dd9516a8404ea49a8de7" => :leopard_g4e
   end
 
   # Tiger's m4 is too old.
