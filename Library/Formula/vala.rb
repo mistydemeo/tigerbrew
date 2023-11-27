c5ca4d24dbe47fec2593257c56ac0d44ef93d230
Misty De Meo <mistydemeo@gmail.com>
2023-11-26 18:51:39 -0800

    vala: help test pass

--- Library/Formula/vala.rb
+++ Library/Formula/vala.rb
@@ -33,7 +33,7 @@ class Vala < Formula
     valac_args = [ # Build with debugging symbols.
       "-g",
       # Use Homebrew's default C compiler.
-      "--cc=#{ENV.cc}",
+      "--cc=#{ENV.cc} -L#{Formula["gettext"].opt_lib}",
       # Save generated C source code.
       "--save-temps",
       # Vala source code path.
