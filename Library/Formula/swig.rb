f677f7a227f24136db69e30d829011a8bd2ee187
Sevan Janiyan <venture37@geeklan.co.uk>
2023-11-27 01:55:45 +0000

    swig 4.1.1 (#860)
    Switch to using pcre2 as pcre is EoL

--- Library/Formula/swig.rb
+++ Library/Formula/swig.rb
@@ -1,19 +1,12 @@
 class Swig < Formula
   desc "Generate scripting interfaces to C/C++ code"
   homepage "http://www.swig.org/"
-  url "https://downloads.sourceforge.net/project/swig/swig/swig-3.0.7/swig-3.0.7.tar.gz"
-  sha256 "06dc8816a225667ce1eee545af3caf87e1bbaa379c32838d4cea53152514348d"
-
-  bottle do
-    sha256 "1290314a7e3c7447a17e16dcc7dbdbc2a090293b4b1c6170f2a4893ccf4fdb43" => :el_capitan
-    sha256 "0dd343668d19966199600282904d3ba799234355f35e0bea56a5e2b278d1ab25" => :yosemite
-    sha256 "c2b2635904546125d52fd7ab452fafb522567d12bd1c98f045d6499dabb8d38d" => :mavericks
-    sha256 "4e4f96d939d6624dbe25902c139fbe29424f7e1a71d06a15f04fefd0a1348f9f" => :mountain_lion
-  end
+  url "http://prdownloads.sourceforge.net/swig/swig-4.1.1.tar.gz"
+  sha256 "2af08aced8fcd65cdb5cc62426768914bedc735b1c250325203716f78e39ac9b"
 
   option :universal
 
-  depends_on "pcre"
+  depends_on "pcre2"
 
   def install
     ENV.universal_binary if build.universal?
