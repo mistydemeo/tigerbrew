ba70d920183a32cef86d703bb0abd1e1e7b66428
Misty De Meo <mistydemeo@gmail.com>
2023-11-26 18:45:35 -0800

    json-glib: add bottle

--- Library/Formula/json-glib.rb
+++ Library/Formula/json-glib.rb
@@ -4,6 +4,10 @@ class JsonGlib < Formula
   url "https://download.gnome.org/sources/json-glib/1.2/json-glib-1.2.8.tar.xz"
   sha256 "fd55a9037d39e7a10f0db64309f5f0265fa32ec962bf85066087b83a2807f40a"
 
+  bottle do
+    sha256 "a5aeeaea60e1b74132ec3fdb16ea0586a47e15eca57cd5e4af901495833877f3" => :tiger_altivec
+  end
+
   depends_on "pkg-config" => :build
   depends_on "make" => :build
   depends_on "glib"
