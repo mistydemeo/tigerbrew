6dfc6fd907bc28de83818048f60bb70114d837d9
Sevan Janiyan <venture37@geeklan.co.uk>
2023-11-28 19:03:12 +0000

    libgpg-error 1.20

--- Library/Formula/libgpg-error.rb
+++ Library/Formula/libgpg-error.rb
@@ -1,11 +1,9 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/"
-  url "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.20.tar.bz2"
-  mirror "ftp://ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.20.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.20.tar.bz2"
-  sha256 "3266895ce3419a7fb093e63e95e2ee3056c481a9bc0d6df694cfd26f74e72522"
-  revision 1
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.47.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.47.tar.bz2"
+  sha256 "9e3c670966b96ecc746c28c2c419541e3bcb787d1a73930f5e5f5e1bcbbb9bdb"
 
   bottle do
     cellar :any
@@ -15,6 +13,7 @@ class LibgpgError < Formula
 
   def install
     ENV.universal_binary if build.universal?
+    ENV.append_to_cflags "-D__DARWIN_UNIX03" if MacOS.version == :tiger
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}",
                           "--disable-silent-rules",
                           "--enable-static"
