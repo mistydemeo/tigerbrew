047d5e21e0527e9fc035d7691a0a37672ae4380d
Davide Nastri <d.nastri@gmail.com>
2023-12-08 16:20:38 +0100

    feat: Found working mirror for ppc version

--- Library/Formula/fatsort.rb
+++ Library/Formula/fatsort.rb
@@ -1,8 +1,8 @@
 class Fatsort < Formula
   desc "Sorts FAT16 and FAT32 partitions"
   homepage "http://fatsort.sourceforge.net/"
-  url "https://sourceforge.net/code-snapshots/svn/f/fa/fatsort/code/fatsort-code-r522-trunk.zip"
-  sha256 "341ab8b9dc04b53d47abcb282c7cb027a2bae982e1ded3083d62aa7c260a1bde"
+  url "https://mirrors.aliyun.com/macports/distfiles/fatsort/fatsort-1.5.0.456.tar.xz"
+  sha256 "a835b47814fd30d5bad464b839e9fc404bc1a6f5a5b1f6ed760ce9744915de95"
 
   depends_on "help2man"
 
