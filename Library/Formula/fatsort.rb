d031a13062c4140176c20e079c29c617aae78847
Misty De Meo <mistydemeo@gmail.com>
2023-12-26 20:49:34 -0800

    fatsort: add bottle

--- Library/Formula/fatsort.rb
+++ Library/Formula/fatsort.rb
@@ -4,6 +4,11 @@ class Fatsort < Formula
   url "https://mirrors.aliyun.com/macports/distfiles/fatsort/fatsort-1.5.0.456.tar.xz"
   sha256 "a835b47814fd30d5bad464b839e9fc404bc1a6f5a5b1f6ed760ce9744915de95"
 
+  bottle do
+    cellar :any_skip_relocation
+    cellar "319dd6f027fa9d97dddd49f6434e2d88294147b8f6f6c215220c521097db2a53" => :tiger_altivec
+  end
+
   depends_on "help2man"
 
   def install
