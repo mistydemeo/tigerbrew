3b94c145775c44a12d01ebdd932f98476465f1f4
Davide Nastri <d.nastri@gmail.com>
2023-12-27 05:47:47 +0100

    fatsort fatsort-1.5.0.456 (#1048)
    * fatsort 1.5.tbd
    
    * Update fatsort.rb
    
    * feat: Found working mirror for ppc version
    
    * Update fatsort.rb

--- Library/Formula/fatsort.rb
+++ Library/Formula/fatsort.rb
@@ -1,13 +1,13 @@
 class Fatsort < Formula
   desc "Sorts FAT16 and FAT32 partitions"
   homepage "http://fatsort.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/fatsort/fatsort-1.3.365.tar.gz"
-  sha256 "77acc374b189e80e3d75d3508f3c0ca559f8030f1c220f7cfde719a4adb03f3d"
+  url "https://mirrors.aliyun.com/macports/distfiles/fatsort/fatsort-1.5.0.456.tar.xz"
+  sha256 "a835b47814fd30d5bad464b839e9fc404bc1a6f5a5b1f6ed760ce9744915de95"
 
   depends_on "help2man"
 
   def install
-    system "make", "CC=#{ENV.cc}"
+    system "make", "CC=#{ENV.cc}", "LD=#{ENV.cc}"
     bin.install "src/fatsort"
     man1.install "man/fatsort.1"
   end
