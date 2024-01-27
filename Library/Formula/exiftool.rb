aa77a1b2d565d0c8ca713d3510c6f0be49f0c450
Sevan Janiyan <venture37@geeklan.co.uk>
2024-01-27 23:56:28 +0000

    exiftool 12.74

--- Library/Formula/exiftool.rb
+++ Library/Formula/exiftool.rb
@@ -1,14 +1,11 @@
 class Exiftool < Formula
   desc "Perl lib for reading and writing EXIF metadata"
-  homepage "http://www.sno.phy.queensu.ca/~phil/exiftool/index.html"
-  url "https://web.archive.org/web/20151024082135/http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.01.tar.gz"
-  sha256 "46b6418334b2ca4e3b21cd38a6cb88131f5256cd21472160b4d19ac078093ceb"
+  homepage "https://exiftool.org/index.html"
+  url "https://exiftool.org/Image-ExifTool-12.74.tar.gz"
+  sha256 "aedb28b1427c53205ab261fa31ff3feda73e7f17a0c181453651680e5666c48a"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f114919fcb753ee3f0894b2abb081211bc2e8e86563ee9ded3db0c9f90e1f7fa" => :el_capitan
-    sha256 "279660c10e3436ff9f25c692bd0287f63a4df4ce332fc794c5173409041b5873" => :yosemite
-    sha256 "ef420fd3dd314fda1a1f94a7e2a20343955995b9cc81f47bb266633358d4b3cb" => :mavericks
   end
 
   def install
