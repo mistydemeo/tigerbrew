75e319a4c9eb5a12b8c786e530017fdbe98efb03
Sevan Janiyan <venture37@geeklan.co.uk>
2024-01-31 20:44:11 +0000

    dfu-programmer 1.1.0

--- Library/Formula/dfu-programmer.rb
+++ Library/Formula/dfu-programmer.rb
@@ -1,14 +1,11 @@
 class DfuProgrammer < Formula
   desc "Device firmware update based USB programmer for Atmel chips"
-  homepage "http://dfu-programmer.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/dfu-programmer/dfu-programmer/0.7.2/dfu-programmer-0.7.2.tar.gz"
-  sha256 "1db4d36b1aedab2adc976e8faa5495df3cf82dc4bf883633dc6ba71f7c4af995"
+  homepage "http://dfu-programmer.github.io"
+  url "https://github.com/dfu-programmer/dfu-programmer/releases/download/v1.1.0/dfu-programmer-1.1.0.tar.gz"
+  sha256 "844e469be559657bc52c9d9d03c30846acd11ffbb1ddd42438fa8af1d2b8587d"
 
   bottle do
     cellar :any
-    sha256 "4dea1ba0456ff657f6bc332db3040d1f9955a1845fcf8d34585187d67637c39e" => :yosemite
-    sha256 "f7e6ab4ed28bf63c21b76917c71ada3675e312475eba15ae1f5f7a5fede3e872" => :mavericks
-    sha256 "a1c904a689bc6b175ac3f2ad2da6d53e11039f3ce55efee77fbcfece2f9e2c24" => :mountain_lion
   end
 
   head do
@@ -17,7 +14,7 @@ class DfuProgrammer < Formula
     depends_on "autoconf" => :build
   end
 
-  depends_on "libusb-compat"
+  depends_on "libusb"
 
   def install
     system "./bootstrap.sh" if build.head?
