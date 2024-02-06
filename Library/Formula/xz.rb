8e015410cf3c7eee55d2fec70815289bc82d4708
Sevan Janiyan <venture37@geeklan.co.uk>
2024-02-06 16:35:43 +0000

    xz 5.4.6

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -3,13 +3,11 @@
 class Xz < Formula
   desc "General-purpose data compression with high compression ratio"
   homepage "http://tukaani.org/xz/"
-  # url "https://fossies.org/linux/misc/xz-5.4.4.tar.gz"
-  # mirror "http://tukaani.org/xz/xz-5.4.4.tar.gz"
-  url "https://www.geeklan.co.uk/files/tmp/xz-5.4.4.tar.gz"
-  sha256 "aae39544e254cfd27e942d35a048d592959bd7a79f9a624afb0498bb5613bdf8"
+  url "https://fossies.org/linux/misc/xz-5.4.6.tar.gz"
+  mirror "http://tukaani.org/xz/xz-5.4.6.tar.gz"
+  sha256 "aeba3e03bf8140ddedf62a0a367158340520f6b384f75ca6045ccc6c0d43fd5c"
 
   bottle do
-    sha256 "27d90bf43274860a39b46d1f589e6386fd5a74e408d0c7c77c89c14bb54adcc7" => :tiger_altivec
   end
 
   option :universal
