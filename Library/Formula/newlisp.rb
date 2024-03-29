fd92908ebdfb1ac87326cb38ff833ccee6050b5d
Sevan Janiyan <venture37@geeklan.co.uk>
2024-03-29 00:09:14 +0000

    newlisp 10.7.5

--- Library/Formula/newlisp.rb
+++ Library/Formula/newlisp.rb
@@ -3,24 +3,17 @@ require "formula"
 class Newlisp < Formula
   desc "Lisp-like, general-purpose scripting language"
   homepage "http://www.newlisp.org/"
-  url "http://www.newlisp.org/downloads/newlisp-10.6.2.tgz"
-  sha1 "8ea722f2ed415548a0904ef15bafd259d8b07e01"
-  revision 1
+  url "http://www.newlisp.org/downloads/newlisp-10.7.5.tgz"
+  sha256 "dc2d0ff651c2b275bc4af3af8ba59851a6fb6e1eaddc20ae75fb60b1e90126ec"
 
   bottle do
-    sha1 "3201cfe276549f314eb8bd429d849277fd43293b" => :yosemite
-    sha1 "6a5503849e0d9ad6a28af27e75396f22fb472ed0" => :mavericks
-    sha1 "bb63e424cc5b4c2caa0c9f414178705c557c32d7" => :mountain_lion
   end
 
-  devel do
-    url "http://www.newlisp.org/downloads/development/inprogress/newlisp-10.6.3.tgz"
-    sha1 "15fff9bff3eb4bb2118b1941ffd34255b9a9a5b5"
-  end
+  # Help find ffi.h
+  patch :p0, :DATA
 
+  depends_on "libffi"
   depends_on "readline"
-
-  patch :DATA
 
   def install
     # Required to use our configuration
@@ -48,32 +41,15 @@ class Newlisp < Formula
     assert_equal "hello\n", shell_output("#{bin}/newlisp #{path}")
   end
 end
-
 __END__
-
---- a/guiserver/newlisp-edit.lsp
-+++ b/Users/gordy/tmp/newlisp-edit
-@@ -1,4 +1,4 @@
--#!/usr/bin/newlisp
-+#!/usr/bin/env newlisp
+--- newlisp.h.orig	2024-03-29 00:00:58.000000000 +0000
++++ newlisp.h	2024-03-29 00:01:22.000000000 +0000
+@@ -111,7 +111,7 @@
+ #ifdef FFI
  
- ; newlisp-edit.lsp - multiple tab LISP editor and support for running code from the editor
- ; needs 9.9.2 version minimum to run
-@@ -157,7 +157,7 @@
- 			(write-file file (base64-dec text)))
- 		(if (= ostype "Win32")
- 			(catch (exec (string {newlisp.exe "} currentScriptFile {" } file " > " (string file "out"))) 'result)
--			(catch (exec (string "/usr/bin/newlisp " currentScriptFile " " file)) 'result)
-+			(catch (exec (string "/usr/local/bin/newlisp " currentScriptFile " " file)) 'result)
- 		)
- 		(if (list? result)
- 			(begin
-@@ -225,7 +225,7 @@
- 		(gs:run-shell 'OutputArea 
- 			(string newlispDir "/newlisp.exe") (string currentExtension " -C -w \"" $HOME "\""))
- 		(gs:run-shell 'OutputArea 
--			(string "/usr/bin/newlisp") (string currentExtension " -C -w " $HOME))
-+			(string "/usr/local/bin/newlisp") (string currentExtension " -C -w " $HOME))
- 	)
- )
+ #if defined(MAC_OSX)
+-#include <ffi/ffi.h>
++#include <ffi.h>
+ #endif
  
+ #if defined(WINDOWS)  
