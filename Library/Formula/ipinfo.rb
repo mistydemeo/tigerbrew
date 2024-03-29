65e03faf73380294924b362ca6ccb6f997143ef3
Sevan Janiyan <venture37@geeklan.co.uk>
2024-03-29 00:38:34 +0000

    ipinfo 1.2

--- Library/Formula/ipinfo.rb
+++ Library/Formula/ipinfo.rb
@@ -1,14 +1,10 @@
 class Ipinfo < Formula
   desc "Tool for calculation of IP networks"
   homepage "http://kyberdigi.cz/projects/ipinfo/"
-  url "http://kyberdigi.cz/projects/ipinfo/files/ipinfo-1.1.tar.gz"
-  sha256 "9ac70f4fa6e124834819c52ffc0f7b3332ff0ce3948865d084e687a6f58eb010"
+  url "http://kyberdigi.cz/projects/ipinfo/files/ipinfo-1.2.tar.gz"
+  sha256 "19e6659f781a48b56062a5527ff463a29c4dcc37624fab912d1dce037b1ddf2d"
 
   bottle do
-    cellar :any
-    sha1 "9982cd38077f1d542736f5db32f76db350c2d987" => :mavericks
-    sha1 "562457c3ac4bcdd8dc8ab856272d37b16e799b7d" => :mountain_lion
-    sha1 "f7fe28e279d45b77d5e74b5710682e11a7814fe8" => :lion
   end
 
   def install
