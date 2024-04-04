d0fded585a264c4c4d39390ff04cb92552422647
Sevan Janiyan <venture37@geeklan.co.uk>
2024-04-04 18:12:34 +0100

    p0f 3.09b

--- Library/Formula/p0f.rb
+++ Library/Formula/p0f.rb
@@ -1,15 +1,13 @@
 class P0f < Formula
   desc "Versatile passive OS fingerprinting, masquerade detection tool"
-  homepage "http://lcamtuf.coredump.cx/p0f3/"
-  url "http://lcamtuf.coredump.cx/p0f3/releases/p0f-3.08b.tgz"
-  sha256 "da89a7fe4ba5db3704207f4bde231643bedd4fe8653ab8111c65766c8c768aec"
+  homepage "https://lcamtuf.coredump.cx/p0f3/"
+  url "https://lcamtuf.coredump.cx/p0f3/releases/p0f-3.09b.tgz"
+  sha256 "543b68638e739be5c3e818c3958c3b124ac0ccb8be62ba274b4241dbdec00e7f"
 
   bottle do
-    cellar :any
-    sha256 "7405d2d0d6070223be3cc0f2ed11c8cd52d886b1480931c1a7e9436d297dbbe7" => :yosemite
-    sha256 "113e04c8f1fba685b42620838cb7fa6907431ff8b4f88ed5ab620f67a7a3aca1" => :mavericks
-    sha256 "cced7b6bab2cdd563d47c6734efb170eb23e3295f25eedaf3294d78ad8812999" => :mountain_lion
   end
+
+  depends_on "libpcap"
 
   def install
     inreplace "config.h", "p0f.fp", "#{etc}/p0f/p0f.fp"
