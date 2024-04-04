6528851a0dbbe70c943913c88dd17dbb677dce76
Sevan Janiyan <venture37@geeklan.co.uk>
2024-04-04 17:59:10 +0100

    ppss: fix urls

--- Library/Formula/ppss.rb
+++ Library/Formula/ppss.rb
@@ -1,7 +1,7 @@
 class Ppss < Formula
   desc "Shell script to execute commands in parallel"
-  homepage "http://ppss.googlecode.com"
-  url "https://ppss.googlecode.com/files/ppss-2.97.tgz"
+  homepage "https://code.google.com/p/ppss/"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/ppss/ppss-2.97.tgz"
   sha256 "25d819a97d8ca04a27907be4bfcc3151712837ea12a671f1a3c9e58bc025360f"
 
   def install
