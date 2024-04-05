79e8006bcda24b868b4e1080d42ac3f505c2cd09
Sevan Janiyan <venture37@geeklan.co.uk>
2024-04-05 11:55:35 +0100

    osxutils 1.9.0
    Sync with
    https://github.com/Homebrew/homebrew-core/blob/be982d27247e210cc3d76dc99ef332fe38e6ef7e/Formula/o/osxutils.rb

--- Library/Formula/osxutils.rb
+++ Library/Formula/osxutils.rb
@@ -1,20 +1,13 @@
 class Osxutils < Formula
   desc "CLI access of Mac-specific information, settings, and metadata"
-  homepage "https://github.com/vasi/osxutils"
-  url "https://github.com/vasi/osxutils/archive/v1.8.1.tar.gz"
-  sha256 "26c49f00435d2b58a3768f0712d25afc09139e91994ba2bdaa21915004214186"
+  homepage "https://github.com/specious/osxutils"
+  url "https://github.com/specious/osxutils/archive/refs/tags/v1.9.0.tar.gz"
+  sha256 "9c11d989358ed5895d9af7644b9295a17128b37f41619453026f67e99cb7ecab"
+  license "GPL-2.0"
   head "https://github.com/vasi/osxutils.git"
 
   bottle do
-    cellar :any
-    sha1 "b2b540572693c94922b19b7109fb337bc45ffddf" => :yosemite
-    sha1 "1f29480eab579c4f97606aabe89d8b39c1c440dd" => :mavericks
-    sha1 "a76a3906847c9bdfa934ee46c7bcdeee772f1c26" => :mountain_lion
   end
-
-  conflicts_with "trash", :because => "both install a trash binary"
-  conflicts_with "leptonica",
-    :because => "both leptonica and osxutils ship a `fileinfo` executable."
 
   def install
     system "make"
@@ -22,6 +15,6 @@ class Osxutils < Formula
   end
 
   test do
-    system "#{bin}/trash"
+    assert_match "osxutils", shell_output("#{bin}/osxutils")
   end
 end
