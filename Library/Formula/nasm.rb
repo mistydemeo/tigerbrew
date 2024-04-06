dfb40a4352a880ca512d43f2c7dcbce023661119
Sevan Janiyan <venture37@geeklan.co.uk>
2024-04-06 21:37:20 +0100

    nasm 2.16.02

--- Library/Formula/nasm.rb
+++ Library/Formula/nasm.rb
@@ -1,15 +1,10 @@
 class Nasm < Formula
   desc "Netwide Assembler (NASM) is an 80x86 assembler"
-  homepage "http://www.nasm.us/"
-  url "http://www.nasm.us/pub/nasm/releasebuilds/2.11.08/nasm-2.11.08.tar.xz"
-  sha256 "c99467c7072211c550d147640d8a1a0aa4d636d4d8cf849f3bf4317d900a1f7f"
+  homepage "https://www.nasm.us"
+  url "https://www.nasm.us/pub/nasm/releasebuilds/2.16.02/nasm-2.16.02.tar.xz"
+  sha256 "1e1b942ea88f22edae89659e15be26fa027eae0747f51413540f52d4eac4790d"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "46bac3d011b8dcfc4874eac8f99af92cef26bbecc486beed6fe809b63d77ca87" => :el_capitan
-    sha256 "4b7434920425f190c807863d79f653d1f3a8668916f5780a5f2c529056d714b9" => :yosemite
-    sha256 "dbcbfe3dd3f67dce50f6baf2a0aea7021f4c893863b4ed552542685488519b38" => :mavericks
-    sha256 "0191f8c219a08f1a3207d63e07b736b627f90d80373d400bded4cada29afc184" => :mountain_lion
   end
 
   option :universal
@@ -17,7 +12,7 @@ class Nasm < Formula
   def install
     ENV.universal_binary if build.universal?
     system "./configure", "--prefix=#{prefix}"
-    system "make install install_rdf"
+    system "make install"
   end
 
   test do
