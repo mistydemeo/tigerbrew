253108bbbe17cabe2ff86102276d7c080e69d2a6
Sevan Janiyan <venture37@geeklan.co.uk>
2024-04-21 20:22:40 +0100

    gnu-indent 2.2.13

--- Library/Formula/gnu-indent.rb
+++ Library/Formula/gnu-indent.rb
@@ -1,9 +1,9 @@
 class GnuIndent < Formula
   desc "C code prettifier"
   homepage "https://www.gnu.org/software/indent/"
-  url "http://ftpmirror.gnu.org/indent/indent-2.2.10.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/indent/indent-2.2.10.tar.gz"
-  sha256 "8a9b41be5bfcab5d8c1be74204b10ae78789fc3deabea0775fdced8677292639"
+  url "http://ftpmirror.gnu.org/indent/indent-2.2.13.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/indent/indent-2.2.13.tar.xz"
+  sha256 "1b81ba4e9a006ca8e6eb5cbbe4cf4f75dfc1fc9301b459aa0d40393e85590a0b"
 
   depends_on "gettext"
 
@@ -11,12 +11,8 @@ class GnuIndent < Formula
 
   option "with-default-names", "Do not prepend 'g' to the binary"
 
-  # Fix broken include and missing build dependency
-  patch :DATA
-
   def install
     args = %W[
-      --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
       --mandir=#{man}
@@ -40,29 +36,3 @@ class GnuIndent < Formula
     EOS
   end
 end
-
-__END__
-diff --git a/man/Makefile.in b/man/Makefile.in
-index 76839bc..8a5fc6e 100644
---- a/man/Makefile.in
-+++ b/man/Makefile.in
-@@ -507,7 +507,7 @@ uninstall-man: uninstall-man1
- 	uninstall-man uninstall-man1
- 
- 
--@PACKAGE@.1: ${srcdir}/@PACKAGE@.1.in  ${srcdir}/../doc/@PACKAGE@.texinfo texinfo2man.c  Makefile.am
-+@PACKAGE@.1: ${srcdir}/@PACKAGE@.1.in  ${srcdir}/../doc/@PACKAGE@.texinfo texinfo2man.c  Makefile.am texinfo2man
- 	./texinfo2man ${srcdir}/@PACKAGE@.1.in ${srcdir}/../doc/@PACKAGE@.texinfo > $@
- # Tell versions [3.59,3.63) of GNU make to not export all variables.
- # Otherwise a system limit (for SysV at least) may be exceeded.
-diff --git a/man/texinfo2man.c b/man/texinfo2man.c
-index e7d82e1..c95266f 100644
---- a/man/texinfo2man.c
-+++ b/man/texinfo2man.c
-@@ -1,6 +1,5 @@
- #include <stdio.h>
- #include <stdlib.h>
--#include <malloc.h>
- #include <string.h>
- #include <ctype.h>
- 
