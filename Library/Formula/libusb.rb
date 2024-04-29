903c622d2b14a2fb2a91e004506b329dbe0cc914
Misty De Meo <mistydemeo@gmail.com>
2024-04-28 17:45:55 -0700

    libusb: remove stray hash

--- Library/Formula/libusb.rb
+++ Library/Formula/libusb.rb
@@ -2,7 +2,6 @@ class Libusb < Formula
   desc "Library for USB device access"
   homepage "https://libusb.info/"
   url "https://github.com/libusb/libusb/releases/download/v1.0.23/libusb-1.0.23.tar.bz2"
-  # sha256 "12ce7a61fc9854d1d2a1ffe095f7b5fac19ddba095c259e6067a46500381b5a5"
   sha256 "db11c06e958a82dac52cf3c65cb4dd2c3f339c8a988665110e0d24d19312ad8d"
   license "LGPL-2.1-or-later"
 
