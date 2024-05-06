9f638b576a973a109d0e769905721258b77e2a7f
Misty De Meo <mistydemeo@gmail.com>
2024-05-05 20:38:14 -0700

    ipinfo: add bottle

--- Library/Formula/ipinfo.rb
+++ Library/Formula/ipinfo.rb
@@ -5,6 +5,8 @@ class Ipinfo < Formula
   sha256 "19e6659f781a48b56062a5527ff463a29c4dcc37624fab912d1dce037b1ddf2d"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "60c24c3578e0f1fdb15d9849a723aaf0ea3d88296c124a56cdf2ed49d96d3633" => :tiger_altivec
   end
 
   def install
