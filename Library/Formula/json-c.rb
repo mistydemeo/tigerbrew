147115878873e86dd7a4725213e52451cd51f291
Sevan Janiyan <venture37@geeklan.co.uk>
2024-06-11 22:38:18 +0100

    json-c 0.17

--- Library/Formula/json-c.rb
+++ Library/Formula/json-c.rb
@@ -1,35 +1,44 @@
 class JsonC < Formula
   desc "JSON parser for C"
   homepage "https://github.com/json-c/json-c/wiki"
-  url "https://github.com/json-c/json-c/archive/json-c-0.12-20140410.tar.gz"
-  version "0.12"
-  sha256 "99304a4a633f1ee281d6a521155a182824dd995139d5ed6ee5c93093c281092b"
+  url "https://s3.amazonaws.com/json-c_releases/releases/json-c-0.17.tar.gz"
+  version "0.17"
+  sha256 "7550914d58fb63b2c3546f3ccfbe11f1c094147bd31a69dcd23714d7956159e6"
+  license "MIT"
 
   bottle do
-    cellar :any
-    sha1 "694077944d6c93066e1a64890effcf8f2670cf94" => :tiger_altivec
-    sha1 "0a26ca526c9073fc07552aed153ad889ffad408f" => :leopard_g3
-    sha1 "86b91aa534135162e7642afab394621d44319080" => :leopard_altivec
   end
+
+  # We build without -Werror, allow build on GCC 4.0.1.
+  patch :p0, :DATA
 
   head do
     url "https://github.com/json-c/json-c.git"
-
-    depends_on "libtool" => :build
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
   end
 
   option :universal
 
+  depends_on "cmake" => :build
+
   def install
     ENV.universal_binary if build.universal?
+    ENV.enable_warnings if ENV.compiler == :gcc_4_0
 
-    system "./autogen.sh" if build.head?
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    ENV.deparallelize
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args, "-DDISABLE_WERROR:BOOL=ON"
+      system "make"
+      system "make", "test", "install"
+    end
   end
 end
+__END__
+--- CMakeLists.txt.orig	2024-06-11 22:19:46.000000000 +0100
++++ CMakeLists.txt	2024-06-11 22:20:19.000000000 +0100
+@@ -302,7 +302,6 @@
+ 	endif()
+     set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} -Wall")
+     set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} -Wcast-qual")
+-    set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} -Wno-error=deprecated-declarations")
+     set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} -Wextra")
+     set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} -Wwrite-strings")
+     set(CMAKE_C_FLAGS           "${CMAKE_C_FLAGS} -Wno-unused-parameter")
