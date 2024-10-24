c2ae9f69590625dc802ef1bf2f0b391d4da11709
Sevan Janiyan <venture37@geeklan.co.uk>
2024-10-24 17:38:40 +0100

    libunistring 1.3

--- Library/Formula/libunistring.rb
+++ Library/Formula/libunistring.rb
@@ -1,13 +1,12 @@
 class Libunistring < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "http://ftpmirror.gnu.org/libunistring/libunistring-1.2.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.xz"
-  sha256 "632bd65ed74a881ca8a0309a1001c428bd1cbd5cd7ddbf8cedcd2e65f4dcdc44"
+  url "http://ftpmirror.gnu.org/libunistring/libunistring-1.3.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/libunistring/libunistring-1.3.tar.xz"
+  sha256 "f245786c831d25150f3dfb4317cda1acc5e3f79a5da4ad073ddca58886569527"
 
   bottle do
     cellar :any
-    sha256 "8074558a34abdb2121379dd2df80fc1572d62983f39e750b37e69f20cc8fa35c" => :tiger_altivec
   end
 
   def install
