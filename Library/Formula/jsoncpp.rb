4ff8a67d8005db46bb855d0cd5e3899fb84fe9ca
Sevan Janiyan <venture37@geeklan.co.uk>
2024-11-15 22:24:00 +0000

    jsoncpp 0.10.7

--- Library/Formula/jsoncpp.rb
+++ Library/Formula/jsoncpp.rb
@@ -1,15 +1,10 @@
 class Jsoncpp < Formula
   desc "Library for interacting with JSON"
   homepage "https://github.com/open-source-parsers/jsoncpp"
-  url "https://github.com/open-source-parsers/jsoncpp/archive/0.10.5.tar.gz"
-  sha256 "56afb14d2ef1c52e72771a221346d4b94f0d46d4e67f796bbcaedb176ca823df"
+  url "https://github.com/open-source-parsers/jsoncpp/archive/0.10.7.tar.gz"
+  sha256 "73e235c230708a8ac78ec11b886434a018f89691bd9e7fcf9c3128c8e677b435"
 
   bottle do
-    cellar :any
-    sha256 "a894c99bea07d28cb0a84aa325b8f203c68293e335522a109174eae9cb662dab" => :el_capitan
-    sha256 "23e7bafcd961be747ab26c3d754f5723ba8a86d4915cf31bcaee002618898b18" => :yosemite
-    sha256 "72c73a2a679f8196bed4fb65cfa04cbb16fa7a565901830f34c3a8d5734c3911" => :mavericks
-    sha256 "7d3400af12dbe322fec3aab9894a920e68b711d01e6205767966ad102cbd8e14" => :mountain_lion
   end
 
   depends_on "scons" => :build
