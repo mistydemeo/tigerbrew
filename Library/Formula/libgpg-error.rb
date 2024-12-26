558e39b7d40ab98f0021183fca4dfc5b78786951
Sevan Janiyan <venture37@geeklan.co.uk>
2024-12-26 01:48:30 +0000

    libgpg-error 1.51

--- Library/Formula/libgpg-error.rb
+++ Library/Formula/libgpg-error.rb
@@ -1,12 +1,11 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/"
-  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.47.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.47.tar.bz2"
-  sha256 "9e3c670966b96ecc746c28c2c419541e3bcb787d1a73930f5e5f5e1bcbbb9bdb"
+  url "https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.51.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libgpg-error/libgpg-error-1.51.tar.bz2"
+  sha256 "be0f1b2db6b93eed55369cdf79f19f72750c8c7c39fc20b577e724545427e6b2"
 
   bottle do
-    sha256 "7c535ced4ccce90ce319aaf147b77f96e555d579c7448647aaccefa4afd21436" => :tiger_altivec
   end
 
   option :universal
