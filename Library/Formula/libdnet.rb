6106fc9ddac58454d3e31633f595665e70a40887
Misty De Meo <mistydemeo@gmail.com>
2025-01-14 21:29:49 -0800

    libdnet: needs check to build

--- Library/Formula/libdnet.rb
+++ Library/Formula/libdnet.rb
@@ -9,6 +9,7 @@ class Libdnet < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "check" => :build
   depends_on "libtool" => :build
   depends_on "cython" => :build
 
