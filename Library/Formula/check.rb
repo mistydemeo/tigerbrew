9b8b790bc9b28eb0f4e5a10a0447f97261b14f40
Misty De Meo <mistydemeo@gmail.com>
2025-01-14 21:20:36 -0800

    check 0.15.2

--- Library/Formula/check.rb
+++ Library/Formula/check.rb
@@ -1,15 +1,11 @@
 class Check < Formula
   desc "C unit testing framework"
   homepage "http://check.sourceforge.net/"
-  url "https://downloads.sourceforge.net/project/check/check/0.10.0/check-0.10.0.tar.gz"
-  sha256 "f5f50766aa6f8fe5a2df752666ca01a950add45079aa06416b83765b1cf71052"
+  url "https://github.com/libcheck/check/releases/download/0.15.2/check-0.15.2.tar.gz"
+  sha256 "a8de4e0bacfb4d76dd1c618ded263523b53b85d92a146d8835eb1a52932fa20a"
 
   bottle do
     cellar :any
-    sha256 "2e37bca055ae0bf490b713d90c95a9d81518cae9b884a7e495f521c80b4f062f" => :el_capitan
-    sha256 "bb824d8cb8a74ebbc1eb9d22b8ea15327dfe3ce91151b37c7f41c80939609f52" => :yosemite
-    sha256 "022667ec28a8af0bf6e19f3afbaa144542f1754ebaff5710229484fa8071084e" => :mavericks
-    sha256 "b223c76b5519c5aa3300488b230c5b5b2cbd32d896f2fab0f1a0700a24e1f975" => :mountain_lion
   end
 
   option :universal
