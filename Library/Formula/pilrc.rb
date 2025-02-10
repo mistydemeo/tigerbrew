832bbb0474ade0a27070494f9acb43a391462c7e
Misty De Meo <mistydemeo@gmail.com>
2025-02-09 23:02:40 -0800

    pilrc: add bottle

--- Library/Formula/pilrc.rb
+++ Library/Formula/pilrc.rb
@@ -4,6 +4,10 @@ class Pilrc < Formula
   url "https://downloads.sourceforge.net/project/pilrc/pilrc/3.2/pilrc-3.2.tar.gz"
   sha256 "f3d6ea3c77f5d2a00707f4372a212377ab7bd77b3d68c3db7e28a553b235903f"
 
+  bottle do
+    sha256 "eb934781d16929e8bb8b34629e79111a438612b810dc31094236ff02d688f61c" => :tiger_altivec
+  end
+
   def install
     system "./unix/configure", "--disable-debug",
                           "--disable-dependency-tracking",
