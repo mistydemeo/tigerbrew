61d2f5474d9877c6bfde33c442ca5bcf2fd81de9
Sevan Janiyan <venture37@geeklan.co.uk>
2025-02-14 22:11:10 +0000

    ninja-ide: add Leopard dependency

--- Library/Formula/ninja-ide.rb
+++ Library/Formula/ninja-ide.rb
@@ -4,6 +4,8 @@ class NinjaIde < Formula
   url "https://github.com/ninja-ide/ninja-ide/archive/v2.3.tar.gz"
   sha256 "413988093531f6d1c251a84e7e01450009da75641e20817271cf387bd1fd8d43"
 
+  # FSEvents API first showed up in Leopard
+  depends_on :macos => :leopard
   depends_on :python
   depends_on "MacFSEvents" => [:python, "fsevents"]
   depends_on "pyqt"
