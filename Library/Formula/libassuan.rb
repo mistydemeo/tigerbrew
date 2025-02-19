8cebba3c48e0a559b1da77e09cd4dad46ab10944
Sevan Janiyan <venture37@geeklan.co.uk>
2025-02-19 01:27:39 +0000

    libassuan 3.0.2

--- Library/Formula/libassuan.rb
+++ Library/Formula/libassuan.rb
@@ -1,13 +1,12 @@
 class Libassuan < Formula
   desc "Assuan IPC Library"
   homepage "https://www.gnupg.org/related_software/libassuan/index.en.html"
-  url "https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-3.0.1.tar.bz2"
-  mirror "ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-3.0.1.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-3.0.1.tar.bz2"
-  sha256 "c8f0f42e6103dea4b1a6a483cb556654e97302c7465308f58363778f95f194b1"
+  url "https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-3.0.2.tar.bz2"
+  mirror "ftp://ftp.gnupg.org/gcrypt/libassuan/libassuan-3.0.2.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/libassuan/libassuan-3.0.2.tar.bz2"
+  sha256 "d2931cdad266e633510f9970e1a2f346055e351bb19f9b78912475b8074c36f6"
 
   bottle do
-    sha256 "6525f567c7a900362a4de6a85b1d7dea6c6c58ac77a956b2059b28eb795ebadc" => :tiger_altivec
   end
 
   # Needed for FD_SET(2)
