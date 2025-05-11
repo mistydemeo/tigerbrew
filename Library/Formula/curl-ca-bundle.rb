ea02274c17ec0c0d2e2dcb7f7fb5032e05d481cb
Misty De Meo <mistydemeo@gmail.com>
2025-05-11 10:02:07 -0700

    curl-ca-bundle: add bottle

--- Library/Formula/curl-ca-bundle.rb
+++ Library/Formula/curl-ca-bundle.rb
@@ -7,6 +7,8 @@ class CurlCaBundle < Formula
 
   bottle do
     cellar :any
+    sha256 "4a04c6b23bf5ba131f8cffcb607909cf48857b67e1011140c2150338a75dadc7" => :tiger_g3
+    sha256 "4a04c6b23bf5ba131f8cffcb607909cf48857b67e1011140c2150338a75dadc7" => :tiger_altivec
   end
 
   def install
