548cc9e6cb051436630db3de4b09f19581f6361a
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-05-22 06:30:39 -0700

    update homepage urls: b

--- Library/Formula/babeld.rb
+++ Library/Formula/babeld.rb
@@ -1,6 +1,6 @@
 class Babeld < Formula
   desc "Loop-avoiding distance-vector routing protocol"
-  homepage "http://www.pps.univ-paris-diderot.fr/~jch/software/babel/"
+  homepage "https://www.irif.fr/~jch/software/babel/"
   url "http://www.pps.univ-paris-diderot.fr/~jch/software/files/babeld-1.6.2.tar.gz"
   sha256 "09de0d99684e95466f07be1eb22fa639c8820d8aaaa7a8db2a18a354cb3d0fb7"
   head "https://github.com/jech/babeld.git"
--- Library/Formula/byacc.rb
+++ Library/Formula/byacc.rb
@@ -1,6 +1,6 @@
 class Byacc < Formula
   desc "(Arguably) the best yacc variant"
-  homepage "http://invisible-island.net/byacc/byacc.html"
+  homepage "https://invisible-island.net/byacc/byacc.html"
   url "ftp://invisible-island.net/byacc/byacc-20141128.tgz"
   sha256 "f517fc21f08c1a1f010177357df58fc64eb1131011e5dcd48c19fe44c47198d0"
 
