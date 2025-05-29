a4666b5cb42e3780f252439a68bdc7b9a5c24f07
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-05-29 07:20:44 -0700

    Revert "Squashed commit of the following:"
    This reverts commit 969837b22d13031dff38c5b4fa02fb0d23afab86.

--- Library/Formula/apachetop.rb
+++ Library/Formula/apachetop.rb
@@ -1,29 +1,32 @@
 class Apachetop < Formula
   desc "Top-like display of Apache log"
   homepage "https://github.com/tessus/apachetop"
-  url "https://github.com/tessus/apachetop/releases/download/0.18.4/apachetop-0.18.4.tar.gz"
-  sha256 "34ee3ad380f1f7c055a96296420f15011b522781691137ea4e3a36f6bd195568"
+  url "https://github.com/tessus/apachetop/releases/download/0.23.2/apachetop-0.23.2.tar.gz"
+  sha256 "f94a34180808c3edb24c1779f72363246dd4143a89f579ef2ac168a45b04443f"
 
   head "https://github.com/tessus/apachetop.git"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "ncurses"
+  depends_on "adns"
+  depends_on "pcre2"
+
   def install
-    args = [
-      "--prefix=#{prefix}",
-      "--mandir=#{man}"
-    ]
-
-    args << if MacOS.version <= :tiger then
-      "--with-logfile=/var/log/httpd/access_log"
-    else
-      "--with-logfile=/var/log/apache2/access_log"
-    end
-
-    system "./configure", *args
+    system "./configure", "--prefix=#{prefix}",
+                          "--mandir=#{man}",
+                          "--disable-debug", 
+                          "--disable-dependency-tracking", 
+                          "--libdir=#{lib}",                    
+                          "--with-logfile=/var/log/apache2/access_log",
+                          "--with-adns=#{Formula["adns"].opt_prefix}",
+                          "--with-pcre2=#{Formula["pcre2"].opt_prefix}"
     system "make", "install"
   end
 
   test do
-    output = shell_output("#{bin}/apachetop -v")
-    assert_match "ApacheTop #{version}", output
+    output = shell_output("#{bin}/apachetop -h 2>&1", 1)
+    assert_match "ApacheTop v#{version}", output
   end
 end
