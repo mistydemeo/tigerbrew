d0126287b95881838d94745970f26ff20d4c116a
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-05-29 07:20:52 -0700

    Revert "apachetop: update urls, fix formula"
    This reverts commit b5d44a607542c98d9cf706b5d94e8aac51015eaa.

--- Library/Formula/apachetop.rb
+++ Library/Formula/apachetop.rb
@@ -1,32 +1,44 @@
 class Apachetop < Formula
   desc "Top-like display of Apache log"
-  homepage "https://github.com/tessus/apachetop"
-  url "https://github.com/tessus/apachetop/releases/download/0.23.2/apachetop-0.23.2.tar.gz"
-  sha256 "f94a34180808c3edb24c1779f72363246dd4143a89f579ef2ac168a45b04443f"
+  homepage "http://freecode.com/projects/apachetop"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/a/apachetop/apachetop_0.12.6.orig.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/a/apachetop/apachetop_0.12.6.orig.tar.gz"
+  sha256 "850062414517055eab2440b788b503d45ebe9b290d4b2e027a5f887ad70f3f29"
 
-  head "https://github.com/tessus/apachetop.git"
+  bottle do
+    cellar :any
+    revision 1
+    sha256 "1cfb399a8548e1ac48d7cb61374e23273aa1eb289e49ba452aa2c55641fe5bae" => :yosemite
+    sha256 "78aa56c9141cfc658120edfb27e795cf178067d54f66c79fc752536d8e0335ea" => :mavericks
+    sha256 "d2383e14241b9af39c197462339393463ae6f8161dae508f49b0753dff846287" => :mountain_lion
+  end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "pkg-config" => :build
-  depends_on "ncurses"
-  depends_on "adns"
-  depends_on "pcre2"
+  # Freecode is officially static from this point forwards. Do not rely on it for up-to-date package information.
+  # Upstream hasn't had activity in years, patch from MacPorts
+  patch :p0, :DATA
 
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
-                          "--disable-debug", 
-                          "--disable-dependency-tracking", 
-                          "--libdir=#{lib}",                    
-                          "--with-logfile=/var/log/apache2/access_log",
-                          "--with-adns=#{Formula["adns"].opt_prefix}",
-                          "--with-pcre2=#{Formula["pcre2"].opt_prefix}"
+                          "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--with-logfile=/var/log/apache2/access_log"
     system "make", "install"
   end
-
-  test do
-    output = shell_output("#{bin}/apachetop -h 2>&1", 1)
-    assert_match "ApacheTop v#{version}", output
-  end
 end
+
+__END__
+--- src/resolver.h    2005-10-15 18:10:01.000000000 +0200
++++ src/resolver.h        2007-02-17 11:24:37.000000000 
+0100
+@@ -10,8 +10,8 @@
+ class Resolver
+ {
+ 	public:
+-	Resolver::Resolver(void);
+-	Resolver::~Resolver(void);
++	Resolver(void);
++	~Resolver(void);
+ 	int add_request(char *request, enum resolver_action act);
+ 
+ 
