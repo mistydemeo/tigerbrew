7a5e37a8a7888fb08835f9de988cc4b171791365
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-06-21 08:49:51 -0700

    Wayback Machine for missing homepages with no redirect: 'x'

--- Library/Formula/xml2.rb
+++ Library/Formula/xml2.rb
@@ -1,6 +1,6 @@
 class Xml2 < Formula
   desc "Makes XML and HTML more amenable to classic UNIX text tools"
-  homepage "http://ofb.net/~egnor/xml2/"
+  homepage "https://web.archive.org/web/20160625044627/http://ofb.net/~egnor/xml2/"
   url "http://download.ofb.net/gale/xml2-0.5.tar.gz"
   sha256 "e3203a5d3e5d4c634374e229acdbbe03fea41e8ccdef6a594a3ea50a50d29705"
 
