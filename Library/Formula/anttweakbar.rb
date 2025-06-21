9e6d48ce52756b6a80ccbc74ccaeaddb7d8e268e
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-06-21 09:18:43 -0700

    anttweakbar: update homepage url

--- Library/Formula/anttweakbar.rb
+++ Library/Formula/anttweakbar.rb
@@ -1,6 +1,6 @@
 class Anttweakbar < Formula
   desc "C/C++ library for adding GUIs to OpenGL apps"
-  homepage "http://www.antisphere.com/Wiki/tools:anttweakbar"
+  homepage "https://anttweakbar.sourceforge.io/doc/"
   url "https://downloads.sourceforge.net/project/anttweakbar/AntTweakBar_116.zip"
   version "1.16"
   sha256 "fbceb719c13ceb13b9fd973840c2c950527b6e026f9a7a80968c14f76fcf6e7c"
