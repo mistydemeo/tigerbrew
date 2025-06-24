fd0d5d1064fada4b516292f395577f1eb34f9015
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-06-24 06:31:58 -0700

    wm for missing resources: k

--- Library/Formula/knock.rb
+++ Library/Formula/knock.rb
@@ -1,7 +1,7 @@
 class Knock < Formula
   desc "Port-knock server"
   homepage "http://www.zeroflux.org/projects/knock"
-  url "http://www.zeroflux.org/proj/knock/files/knock-0.7.tar.gz"
+  url "https://web.archive.org/web/20190228180646/https://zeroflux.org/proj/knock/files/knock-0.7.tar.gz"
   sha256 "9938479c321066424f74c61f6bee46dfd355a828263dc89561a1ece3f56578a4"
 
   bottle do
