766185f54d50f426a75a3e2254ec3bee75f52bf1
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-06-24 06:30:21 -0700

    wm for missing resources: a

--- Library/Formula/aterm.rb
+++ Library/Formula/aterm.rb
@@ -1,7 +1,7 @@
 class Aterm < Formula
   desc "AfterStep terminal emulator"
   homepage "http://strategoxt.org/Tools/ATermFormat"
-  url "http://www.meta-environment.org/releases/aterm-2.8.tar.gz"
+  url "https://web.archive.org/web/20150503094402/http://www.meta-environment.org/releases/aterm-2.8.tar.gz"
   sha256 "bab69c10507a16f61b96182a06cdac2f45ecc33ff7d1b9ce4e7670ceeac504ef"
 
   bottle do
--- Library/Formula/aws-cloudsearch.rb
+++ Library/Formula/aws-cloudsearch.rb
@@ -1,7 +1,7 @@
 class AwsCloudsearch < Formula
   desc "Client for Amazon CloudSearch web service"
   homepage "https://aws.amazon.com/developertools/9054800585729911"
-  url "https://s3.amazonaws.com/amazon-cloudsearch-data/cloud-search-tools-v2-2.0.1.0-2014.10.27.tar.gz"
+  url "https://web.archive.org/web/20160809122222/https://s3.amazonaws.com/amazon-cloudsearch-data/cloud-search-tools-v2-2.0.1.0-2014.10.27.tar.gz"
   version "2.0.1.0-2014.10.27"
   sha256 "882a6442172957b27c0b3cd1f271531112092d4d227b528ce912b2e7ea886d51"
   revision 1
