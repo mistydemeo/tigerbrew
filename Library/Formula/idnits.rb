0fc251ab37278daaa9fd3f2551e258966cc72f30
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-06-24 06:31:39 -0700

    wm for missing resources: i

--- Library/Formula/idnits.rb
+++ Library/Formula/idnits.rb
@@ -1,7 +1,7 @@
 class Idnits < Formula
   desc "Looks for problems in internet draft formatting"
   homepage "https://tools.ietf.org/tools/idnits/"
-  url "https://tools.ietf.org/tools/idnits/idnits-2.13.02.tgz"
+  url "https://web.archive.org/web/20160305194833/https://tools.ietf.org/tools/idnits/idnits-2.13.02.tgz"
   sha256 "6e42b044c79dc4e616d10ee9e283c20acc741601811a6acfc0c0d310afdf0823"
 
   depends_on "aspell"
--- Library/Formula/ipbt.rb
+++ Library/Formula/ipbt.rb
@@ -1,7 +1,7 @@
 class Ipbt < Formula
   desc "Program for recording a UNIX terminal session"
   homepage "http://www.chiark.greenend.org.uk/~sgtatham/ipbt/"
-  url "http://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20141026.2197432.tar.gz"
+  url "https://web.archive.org/web/20160513031554/https://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20141026.2197432.tar.gz"
   sha256 "151da94da378cc88e979df8eb5f9a05c4e663bd1299c191d24c10128bae879b0"
 
   bottle do
