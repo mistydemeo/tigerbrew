09c76835078b8f80efede7eb45275d1b7cf1e2e6
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-06-24 06:34:24 -0700

    wm for missing resources: z

--- Library/Formula/zsh-lovers.rb
+++ Library/Formula/zsh-lovers.rb
@@ -1,7 +1,7 @@
 class ZshLovers < Formula
   desc "Tips, tricks, and examples for zsh"
   homepage "http://grml.org/zsh/#zshlovers"
-  url "http://grml.org/zsh/zsh-lovers.1"
+  url "https://web.archive.org/web/20230425045120/https://grml.org/zsh/zsh-lovers.1"
   version "0.9.1"
   sha256 "6583aabf4024c951f19a381ce97ce2c7af948b24fc332504c3f719ec9187c72f"
 
