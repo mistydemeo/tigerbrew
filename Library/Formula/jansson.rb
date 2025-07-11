4e8f9af379d180e1e0e4739c44dde2dc4b7eaab0
Sevan Janiyan <venture37@geeklan.co.uk>
2025-07-11 21:46:59 +0100

    jansson 2.14.1
    Fix test

--- Library/Formula/jansson.rb
+++ Library/Formula/jansson.rb
@@ -1,15 +1,12 @@
 class Jansson < Formula
   desc "C library for encoding, decoding, and manipulating JSON"
   homepage "http://www.digip.org/jansson/"
-  url "http://www.digip.org/jansson/releases/jansson-2.7.tar.gz"
-  sha256 "7905e6590fb316c0ff943df3dc6a21cd81a59cff7a6d12514054c359d04d78d7"
+  url "https://github.com/akheron/jansson/releases/download/v2.14.1/jansson-2.14.1.tar.bz2"
+  sha256 "6bd82d3043dadbcd58daaf903d974891128d22aab7dada5d399cb39094af49ce"
+  license "MIT"
 
   bottle do
     cellar :any
-    sha256 "dfcc2f484a285f9a0119e7011702363c5516aaed22a9e4de4d83279e543c98cf" => :el_capitan
-    sha1 "7c9087f9ce0f65276339832bbbf7f6f813eed03d" => :yosemite
-    sha1 "5c0224602ecb036cdc1e636cff2895094fafac04" => :mavericks
-    sha1 "876f6358e0277ddeec6d36c647723452ccf3adfa" => :mountain_lion
   end
 
   option :universal
@@ -36,7 +33,7 @@ class Jansson < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-ljansson", "-o", "test"
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-ljansson", "-o", "test"
     system "./test"
   end
 end
