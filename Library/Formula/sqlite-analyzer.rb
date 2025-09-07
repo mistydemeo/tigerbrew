20fa8b5b65610344dc7b63b8a8232d781343679e
Sevan Janiyan <venture37@geeklan.co.uk>
2025-09-07 21:15:51 +0100

    sqlite-analyzer 3.50.4
    Need to use a version of TCL newer than provided by Tiger.

--- Library/Formula/sqlite-analyzer.rb
+++ Library/Formula/sqlite-analyzer.rb
@@ -1,19 +1,19 @@
 class SqliteAnalyzer < Formula
   desc "Analyze how space is allocated inside an SQLite file"
   homepage "https://www.sqlite.org/"
-  url "https://www.sqlite.org/2015/sqlite-src-3080803.zip"
-  version "3.8.8.3"
-  sha256 "790ff6be164488d176b3bed7e0e0850bac1567a4011381307685d48eb69fab48"
+  url "https://www.sqlite.org/2025/sqlite-src-3500400.zip"
+  version "3.50.4"
+  sha256 "b7b4dc060f36053902fb65b344bbbed592e64b2291a26ac06fe77eec097850e9"
+  license "blessing"
 
   bottle do
     cellar :any
-    sha256 "9eb7d3df9680d81826da6e39041d28abeba247cbb084a8690524485d34a934c7" => :yosemite
-    sha256 "5092ae2ccce00d9dbf4b8d33a318883054eafd904d55e0ba8ef6a415f7b31497" => :mavericks
-    sha256 "abaf9bc21f00454ee737716345f633383beb92bda83f45c27420eebab3d4d5b7" => :mountain_lion
   end
 
+  depends_on "tcl-tk"
+
   def install
-    system "./configure", "--disable-debug", "--prefix=#{prefix}"
+    system "./configure", "--with-tcl=#{Formula["tcl-tk"].opt_lib}", "--disable-debug", "--prefix=#{prefix}"
     system "make", "sqlite3_analyzer"
     bin.install "sqlite3_analyzer"
   end
