81c9219679f5583090ce70d1ddb84082bb28c60c
Sevan Janiyan <venture37@geeklan.co.uk>
2025-11-09 21:46:57 +0000

    arm-none-eabi-gdb 14.2

--- Library/Formula/arm-none-eabi-gdb.rb
+++ Library/Formula/arm-none-eabi-gdb.rb
@@ -1,12 +1,11 @@
 class ArmNoneEabiGdb < Formula
   desc "GNU debugger for arm-none-eabi cross development"
   homepage "https://www.gnu.org/software/gdb/"
-  url "https://ftp.gnu.org/gnu/gdb/gdb-14.1.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gdb/gdb-14.1.tar.xz"
-  sha256 "d66df51276143451fcbff464cc8723d68f1e9df45a6a2d5635a54e71643edb80"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-14.2.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-14.2.tar.xz"
+  sha256 "2d4dd8061d8ded12b6c63f55e45344881e8226105f4d2a9b234040efa5ce7772"
   license "GPL-3.0-or-later"
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
-  revision 1
 
   bottle do
   end
