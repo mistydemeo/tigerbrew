d214961c07e38e5ac4b2d4f5f4d2d5fc15a0dfbd
a1batross <a1ba.omarov@gmail.com>
2025-11-19 13:14:10 +0500

    zstd: patch to compile with 4.0, use newer cctools
    There are two needed changes. The first one is to assign something to dummy global variable in `lib/common/threading.c` to avoid ld freak out with following message:
    /usr/bin/ld: common symbols not allowed with MH_DYLIB output format with the -multi_module option
    obj/conf_31f44aca5c9e51b61f3105be9dd83144/dynamic/threading.o private external definition of common _g_ZSTD_threading_useless_symbol (size 4)
    
    The second change only removes unsupported diagnostic flags.

--- Library/Formula/zstd.rb
+++ Library/Formula/zstd.rb
@@ -10,14 +10,47 @@ class Zstd < Formula
   ]
 
   # 4.0 doesn't support -Wvla -Wc++-compat
-  # 4.2 doesn't support -Wvla
-  fails_with :gcc
+  # these are just enable diagnostic messages
+  # so remove them
+  patch :p1, :DATA
 
   depends_on "make" => :build
+  depends_on "cctools" => :build
 
   def install
+    ENV["AS"] = Formula["cctools"].bin/"as"
     system "gmake", "clean"
     system "gmake", "BACKTRACE=0"
     system "gmake", "install", "PREFIX=#{prefix}"
   end
 end
+__END__
+diff --git a/lib/common/threading.c b/lib/common/threading.c
+index 25bb8b9..7f630fa 100644
+--- a/lib/common/threading.c
++++ b/lib/common/threading.c
+@@ -18,7 +18,7 @@
+ #include "threading.h"
+ 
+ /* create fake symbol to avoid empty translation unit warning */
+-int g_ZSTD_threading_useless_symbol;
++int g_ZSTD_threading_useless_symbol = 0;
+ 
+ #if defined(ZSTD_MULTITHREAD) && defined(_WIN32)
+ 
+diff --git a/lib/libzstd.mk b/lib/libzstd.mk
+index 91bd4ca..a7d13dc 100644
+--- a/lib/libzstd.mk
++++ b/lib/libzstd.mk
+@@ -113,8 +113,8 @@ endif
+ DEBUGFLAGS= -Wall -Wextra -Wcast-qual -Wcast-align -Wshadow \
+             -Wstrict-aliasing=1 -Wswitch-enum -Wdeclaration-after-statement \
+             -Wstrict-prototypes -Wundef -Wpointer-arith \
+-            -Wvla -Wformat=2 -Winit-self -Wfloat-equal -Wwrite-strings \
+-            -Wredundant-decls -Wmissing-prototypes -Wc++-compat
++            -Wformat=2 -Winit-self -Wfloat-equal -Wwrite-strings \
++            -Wredundant-decls -Wmissing-prototypes
+ CFLAGS   += $(DEBUGFLAGS) $(MOREFLAGS)
+ ASFLAGS  += $(DEBUGFLAGS) $(MOREFLAGS) $(CFLAGS)
+ LDFLAGS  += $(MOREFLAGS)
+
