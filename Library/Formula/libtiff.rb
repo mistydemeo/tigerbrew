53f25c856ff58978545d28356334ea8cd7a15677
Sevan Janiyan <venture37@geeklan.co.uk>
2025-12-02 19:32:06 +0000

    libiff 4.7.1

--- Library/Formula/libtiff.rb
+++ Library/Formula/libtiff.rb
@@ -1,11 +1,10 @@
 class Libtiff < Formula
   desc "TIFF library and utilities"
   homepage "https://libtiff.gitlab.io/libtiff/"
-  url "https://download.osgeo.org/libtiff/tiff-4.6.0.tar.xz"
-  sha256 "e178649607d1e22b51cf361dd20a3753f244f022eefab1f2f218fc62ebaf87d2"
+  url "https://download.osgeo.org/libtiff/tiff-4.7.1.tar.xz"
+  sha256 "b92017489bdc1db3a4c97191aa4b75366673cb746de0dce5d7a749d5954681ba"
 
   bottle do
-    sha256 "bb0a906ad2162c37c63d8222584cec44e712b5478d7a45d931f6782bcc05f695" => :tiger_altivec
   end
 
   option :universal
@@ -20,6 +19,10 @@ class Libtiff < Formula
   end
 
   def install
+    # tif_dir.c:855: error: for loop initial declaration used outside C99 mode
+    # tif_dir.c:874: error: for loop initial declaration used outside C99 mode
+    ENV.append "CFLAGS", "-std=gnu99"
+
     ENV.universal_binary if build.universal?
     ENV.cxx11 if build.cxx11?
     jpeg = Formula["jpeg"].opt_prefix
