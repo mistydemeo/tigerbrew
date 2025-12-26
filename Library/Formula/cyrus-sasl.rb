9216073492b151fd87d111adb410bf2dbaf47d72
Sevan Janiyan <venture37@geeklan.co.uk>
2025-12-26 23:33:18 +0000

    cyrus-sasl: Switch to OpenSSL 3
    Formula was last updated before OpenSSL 3 landed.

--- Library/Formula/cyrus-sasl.rb
+++ Library/Formula/cyrus-sasl.rb
@@ -6,12 +6,11 @@ class CyrusSasl < Formula
   sha256 "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"
 
   bottle do
-    sha256 "e2387b76b95e32bf97a0f1433eb882225b2d027eb4c3717a37afc463225a885b" => :tiger_altivec
   end
 
   keg_only :provided_by_osx
 
-  depends_on "openssl"
+  depends_on "openssl3"
 
   def install
     system "./configure",
