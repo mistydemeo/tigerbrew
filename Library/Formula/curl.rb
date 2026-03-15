b3dc5568047095015b6f29c1cfd038e92310df7c
Sevan Janiyan <venture37@geeklan.co.uk>
2026-03-15 16:38:01 +0000

    curl 8.19.0

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -1,14 +1,13 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
-  url "https://curl.se/download/curl-8.17.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-8_17_0/curl-8.17.0.tar.bz2"
-  sha256 "230032528ce5f85594d4f3eace63364c4244ccc3c801b7f8db1982722f2761f4"
+  url "https://curl.se/download/curl-8.19.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-8_19_0/curl-8.19.0.tar.bz2"
+  sha256 "eba3230c1b659211a7afa0fbf475978cbf99c412e4d72d9aa92d020c460742d4"
   license "curl"
 
   bottle do
     cellar :any
-    sha256 "87626ad056ad83a6345af6641c6be41f679aeee6d9d81fc169c92b25bb439869" => :tiger_g3
   end
 
   keg_only :provided_by_osx
