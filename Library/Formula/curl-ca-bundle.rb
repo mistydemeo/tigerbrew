3eb5c7a584b5f4992f20c3d6f789641b8a9927e3
Sevan Janiyan <venture37@geeklan.co.uk>
2026-06-24 13:20:38 +0100

    curl-ca-bundle 2026-05-14

--- Library/Formula/curl-ca-bundle.rb
+++ Library/Formula/curl-ca-bundle.rb
@@ -1,9 +1,9 @@
 class CurlCaBundle < Formula
   homepage "http://curl.haxx.se/docs/caextract.html"
-  url "https://curl.se/ca/cacert-2025-12-02.pem",
+  url "https://curl.se/ca/cacert-2026-05-14.pem",
     :using => :nounzip
-  sha256 "f1407d974c5ed87d544bd931a278232e13925177e239fca370619aba63c757b4"
-  version "2025-12-02"
+  sha256 "86a1f3366afac7c6f8ae9f3c779ac221129328c43f0ab2b8817eb2f362a5025c"
+  version "2026-05-14"
 
   bottle do
     cellar :any
